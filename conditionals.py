908a71d341ea3971845e70ff563e7ecc0d8ccee2
risandiro <151243329+risandiro@users.noreply.github.com>
2023-11-21 17:53:57 +0000

    Tue, Nov 21, 2023, 6:53 PM +01:00

--- conditionals.py
+++ conditionals.py
@@ -3,8 +3,8 @@ y = int(input("What's y? "))
 
 if x < y:
     print("x is less than y")
-if x > y:
+elif x > y:
     print("x is greater than y")
-if x == y:
+elif x == y:
     print("x is equal to y")
 
