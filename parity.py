92ecc2c7de39563c32895573d1d4995e98040c9b
risandiro <151243329+risandiro@users.noreply.github.com>
2023-11-21 18:49:12 +0000

    Tue, Nov 21, 2023, 7:49 PM +01:00

--- parity.py
+++ parity.py
@@ -1,3 +1,11 @@
-x = int(input("What's x: "))
+def main():
+    x = int(input("What's x? "))
+    if is_even(x):
+        print("Even")
+    else
+        print("Odd")
 
-if x  
+def is_even(n):
+    if n  % 2 == 0:
+
+main()
