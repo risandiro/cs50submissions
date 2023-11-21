fff22ccf3fd55c82d29c9cccc341789bb21c571c
risandiro <151243329+risandiro@users.noreply.github.com>
2023-11-21 20:32:31 +0000

    Tue, Nov 21, 2023, 9:32 PM +01:00

--- house.py
+++ house.py
@@ -1,7 +1,9 @@
 name = input("What's your name? ")
 
 match name:
-    case "Harry":
+    case "Harry" | "Hermione" | "Ron":
         print ("Gryffindor")
+    case "Draco:
+        print("Slytherin")
     case _:
         print("Who?")
