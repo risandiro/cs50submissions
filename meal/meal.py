e0c1063f4eba67a8e30640f82c477910d826044e
risandiro <151243329+risandiro@users.noreply.github.com>
2023-11-22 17:10:10 +0000

    Wed, Nov 22, 2023, 6:10 PM +01:00

--- meal/meal.py
+++ meal/meal.py
@@ -11,9 +11,8 @@ def convert(time):
     hours, minutes = time.split(":")
     hours = float(hours)
     minutes = float(minutes)
-    if 0 <= hours <= 23 and 0 <= minutes <= 59:
-        hours  = hours + (minutes / 60)
-        return hours
+    hours  = hours + (minutes / 60)
+    return hours
 
 if __name__ == "__main__":
     main()
