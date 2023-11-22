18dbdd711058a853d9f737731981656c2871c80d
risandiro <151243329+risandiro@users.noreply.github.com>
2023-11-22 14:09:32 +0000

    Wed, Nov 22, 2023, 3:09 PM +01:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -2,8 +2,10 @@ x = input("File name: ")
 x = x.name.strip()
 x = x.lower()
 
-if y = x.endswith(".gif" or ".jpg" or ".jpeg" or ".png" or ".pdf" or ".txt" or ".zip") == True:
+if x.endswith(".gif" or ".jpg" or ".jpeg" or ".png" or ".pdf" or ".txt" or ".zip") == True:
+    match x:
+        case ".gif"
 
 else:
-    print()
+    print("application/octet-stream")
 
