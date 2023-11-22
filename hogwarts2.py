2853416f9def514419e720b47f50dff428049265
risandiro <151243329+risandiro@users.noreply.github.com>
2023-11-22 20:15:07 +0000

    Wed, Nov 22, 2023, 9:15 PM +01:00

---
+++ hogwarts2.py
@@ -0,0 +1,7 @@
+students = [
+    {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"}
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russel terrier"}
+    {"name": "Draco", "house": "Slytherin", "patronus": "None"}
+    # 'None' represents the absense of a value
+]
