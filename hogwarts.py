b3c2b5c6606ff5d47e094c642e085bc7e9a551e3
risandiro <151243329+risandiro@users.noreply.github.com>
2023-11-22 19:49:35 +0000

    Wed, Nov 22, 2023, 8:49 PM +01:00

--- hogwarts.py
+++ hogwarts.py
@@ -12,6 +12,8 @@ students = ["Hermione", "Harry", "Ron"]
 for i in range(len(students)):
     print(i + 1, students[i])
 
+print("\n-----------------------\n")
+
 # a 'dictionary' variable
 # "key": "variable"
 students2 = {
@@ -22,4 +24,4 @@ students2 = {
 }
 
 for student in students2:
-    print(student)
+    print(student, students2[student], sep=", ")
