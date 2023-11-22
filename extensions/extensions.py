1582b178781b483b5da4828c673f29670dd4f6d2
risandiro <151243329+risandiro@users.noreply.github.com>
2023-11-22 14:06:51 +0000

    Wed, Nov 22, 2023, 3:06 PM +01:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -2,5 +2,8 @@ x = input("File name: ")
 x = x.name.strip()
 x = x.lower()
 
-y = x.endswith(".gif" or ".jpg" or ".jpeg" or ".png" or ".pdf" or ".txt" or ".zip")
+if y = x.endswith(".gif" or ".jpg" or ".jpeg" or ".png" or ".pdf" or ".txt" or ".zip") == True:
+
+else:
+    print()
 
