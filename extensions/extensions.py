a43c4fd123f998d3c52364996844539cc29c2747
risandiro <151243329+risandiro@users.noreply.github.com>
2023-11-22 14:16:44 +0000

    Wed, Nov 22, 2023, 3:16 PM +01:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -5,9 +5,16 @@ x = x.lower()
 if x.endswith(".gif"):
     print("image/gif")
 elif x.endswith(".jpg" or ".jpeg"):
-    print("image/jpeg)
+    print("image/jpeg")
 elif x.endswith(".png"):
     print("image/png")
-
+elif x.endswith(".pdf"):
+    print("application/pdf")
+elif x.endswith(".txt"):
+    print("text/plain")
+elif x.endswith(".zip"):
+    print("application/zip")
+else:
+    print("application/octet-stream")
 
 
