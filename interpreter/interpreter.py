13f74c492dd592c45e40253aed9a486e24d4d630
risandiro <151243329+risandiro@users.noreply.github.com>
2023-11-22 14:58:28 +0000

    Wed, Nov 22, 2023, 3:58 PM +01:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,8 +1,14 @@
 expression = input("Expression: ")
-x, z = int(x)
-int(z)
 x, y, z = expression.split(" ")
+x = int(x)
+z = int(z)
 
 match y:
     case "+":
         print(x + z)
+    case "-":
+        print(x - z)
+    case "*":
+        print(x * z)
+    case "/":
+        print(x / z)
