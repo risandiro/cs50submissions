bbe7771bc58a0d6b09029e99817eda164e54b467
risandiro <151243329+risandiro@users.noreply.github.com>
2023-11-22 19:08:51 +0000

    Wed, Nov 22, 2023, 8:08 PM +01:00

--- hogwarts.py
+++ hogwarts.py
@@ -2,13 +2,13 @@
 students = ["Hermione", "Harry", "Ron"]
 
 # python starts counting from 0, not from 1
-print(students[0], end="\n")
+# print(students[0], end="\n")
 
 # python automatically assigns the variable to be 0 in a 'for' loop
-for student in students:
-    print(student, end="\n")
+# for student in students:
+#    print(student, end="\n")
 
 # command 'len' tells you the lenght of a list
 
 for i in range(len(students)):
-    print(i, student[i])
+    print(i, students[i])
