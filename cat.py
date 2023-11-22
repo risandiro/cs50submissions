3bf6182791117a05e91dd9c99421dda5bdc8e372
risandiro <151243329+risandiro@users.noreply.github.com>
2023-11-22 18:39:44 +0000

    Wed, Nov 22, 2023, 7:39 PM +01:00

--- cat.py
+++ cat.py
@@ -1,21 +1,18 @@
 '''
-print("meow\n" * 3, end="")
-
-While loop:
-
 i = 0
 while i < 3:
     print("meow")
     i += 1
 
-For loops:
-
-for i in [0, 1, 2]:
 for _ in range(3):
     print("meow")
 
 '''
 
-n = int(input("What's n? "))
-if n < 0
+while True:
+    n = int(input("What's n? "))
+    if n > 0:
+        break
 
+for _ in range(n):
+    print("meow")
