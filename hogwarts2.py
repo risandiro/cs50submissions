eb31ad3ef5e4539a6ad3872a7d2de136a5aed6d3
risandiro <151243329+risandiro@users.noreply.github.com>
2023-11-22 20:16:46 +0000

    Wed, Nov 22, 2023, 9:16 PM +01:00

--- hogwarts2.py
+++ hogwarts2.py
@@ -1,7 +1,8 @@
+# a list filled with collection of dictionaries
 students = [
     {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
     {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"}
     {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russel terrier"}
-    {"name": "Draco", "house": "Slytherin", "patronus": "None"}
+    {"name": "Draco", "house": "Slytherin", "patronus": None}
     # 'None' represents the absense of a value
 ]
