b6d931f9c2c9e156b91d6001f0a44b17db32d30b
risandiro <151243329+risandiro@users.noreply.github.com>
2023-11-22 18:58:57 +0000

    Wed, Nov 22, 2023, 7:58 PM +01:00

--- hogwarts.py
+++ hogwarts.py
@@ -1,6 +1,9 @@
+# a 'list' variable
 students = ["Hermione", "Harry", "Ron"]
 
+# python starts counting from 0, not from 1
 print(students[0])
 
+# python automatically assigns the variable to be 0 in a 'for' loop
 for student in students:
     print(student)
