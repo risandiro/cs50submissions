13945e5c9cdd7970f828f31f3f1630e7e6d399bf
risandiro <151243329+risandiro@users.noreply.github.com>
2023-11-22 16:44:47 +0000

    Wed, Nov 22, 2023, 5:44 PM +01:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,14 +1,13 @@
 def main():
     x = input("What time is it? ")
     convert(x)
-    print(hours)
 
 
 def convert(time):
     hours, minutes = time.split(":")
     hours = float(hours)
     minutes = float(minutes)
-    hours = hours + (minutes / 60)
+     = hours + (minutes / 60)
 
 if __name__ == "__main__":
     main()
