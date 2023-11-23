493438fac2a2c8331bf45aa67c226aafbbd24f6f
risandiro <151243329+risandiro@users.noreply.github.com>
2023-11-23 18:51:56 +0000

    Thu, Nov 23, 2023, 7:51 PM +01:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,19 +1,26 @@
 total = 50
+print("Amount Due: 50")
 
-while total >= 0:
+while total > 0:
     coin = input("Insert Coin: ")
 
     match coin:
         case "25":
-            if total >= 0:
+            if total > 0:
                 total = total - 25
                 print("Amount Due:", total)
+            elif total == 0:
+                print("Change Owed:", total)
         case "10":
-            if total >= 0:
+            if total > 0:
                 total = total - 10
                 print("Amount Due:", total)
+            elif total == 0:
+                print("Change Owed:", total)
         case "5":
-            if total >= 0:
+            if total > 0:
                 total = total - 5
                 print("Amount Due:", total)
+            elif total == 0:
+                print("Change Owed:", total)
 
