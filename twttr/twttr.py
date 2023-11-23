a43b0a7d90fd3553a18c818eb48ceff3c37fff85
risandiro <151243329+risandiro@users.noreply.github.com>
2023-11-23 19:28:53 +0000

    Thu, Nov 23, 2023, 8:28 PM +01:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,8 +1,10 @@
 s = input("Input: ")
+vowels = "a", "A", "e", "E", "i", "I", "o", "O", "u", "U"
+
 print("Output: ", end="")
 
 for c in s:
-    if c != "a" or "A":
+    if c != vowels:
         print(c, end="")
 
 print()
