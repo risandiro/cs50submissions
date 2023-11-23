28fecd77c4b0f4d083e55d1bc555174234a62259
risandiro <151243329+risandiro@users.noreply.github.com>
2023-11-23 18:37:41 +0000

    Thu, Nov 23, 2023, 7:37 PM +01:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,17 +1,18 @@
 total = 50
-coin = input("Insert Coin: ")
+while total != 0
+    coin = input("Insert Coin: ")
 
-while total != 0:
-    match coin:
-        case "25":
-            if total != 0:
-                total = total - 25
-                print("Amount Due:", total)
-        case "10":
-            if total != 0:
-                total = total - 10
-                print("Amount Due:", total)
-        case "5":
-            if total != 0:
-                total = total - 5
-                print("Amount Due:", total)
+
+match coin:
+    case "25":
+        if total != 0:
+            total = total - 25
+            print("Amount Due:", total)
+    case "10":
+        if total != 0:
+            total = total - 10
+            print("Amount Due:", total)
+    case "5":
+        if total != 0:
+            total = total - 5
+            print("Amount Due:", total)
