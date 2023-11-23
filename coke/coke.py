d575ee26bf50d0b3fa3bfee8cc9564d5d808773a
risandiro <151243329+risandiro@users.noreply.github.com>
2023-11-23 18:49:11 +0000

    Thu, Nov 23, 2023, 7:49 PM +01:00

--- coke/coke.py
+++ coke/coke.py
@@ -5,16 +5,15 @@ while total >= 0:
 
     match coin:
         case "25":
-            if total != 0:
+            if total >= 0:
                 total = total - 25
                 print("Amount Due:", total)
         case "10":
-            if total != 0:
+            if total >= 0:
                 total = total - 10
                 print("Amount Due:", total)
         case "5":
-            if total != 0:
+            if total >= 0:
                 total = total - 5
                 print("Amount Due:", total)
 
-print("Change Owed:", total)
