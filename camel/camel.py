057b5f4c5b3e8510e067d03f5bad899422c2099f
risandiro <151243329+risandiro@users.noreply.github.com>
2023-11-23 18:01:30 +0000

    Thu, Nov 23, 2023, 7:01 PM +01:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,5 +1,7 @@
-s = input("camelCasse: ")
+s = input("camelCase: ")
+print("snake_case: ", end=" ")
+
 for c in s:
-        if c.isupper():
-            print(_)
+    if c.isupper():
+        print("_", end="")
     print(c, end="")
