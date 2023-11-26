8ee9971ee55272577e94b3fa0028d1cbb5633d76
risandiro <151243329+risandiro@users.noreply.github.com>
2023-11-26 21:28:06 +0000

    Sun, Nov 26, 2023, 10:28 PM +01:00

--- plates/plates.py
+++ plates/plates.py
@@ -6,6 +6,10 @@ def main():
         print("Invalid")
 
 def is_valid(s):
+    rule
+
+
+def rule_one(s):
     first_letter = s[0:1]
     second_letter = s[1:2]
     if first_letter or second_letter
