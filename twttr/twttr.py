51d2769d9986a1e25c111d967318a6b92551673d
risandiro <151243329+risandiro@users.noreply.github.com>
2023-11-26 21:00:32 +0000

    Sun, Nov 26, 2023, 10:00 PM +01:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,8 +1,9 @@
 input = input("Input: ")
 vowels = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"]
 
-for i in range(10):
-    if 
+for letter in input:
+    for i in range(10):
+        if letter =! 
 
 
 
