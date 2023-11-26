7db2a789b93ddbf3b0b80f53fc34e17e77d9b33b
risandiro <151243329+risandiro@users.noreply.github.com>
2023-11-26 21:07:06 +0000

    Sun, Nov 26, 2023, 10:07 PM +01:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,9 +1,16 @@
 input = input("Input: ")
 vowels = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"]
+counter = 0
 
 for letter in input:
     for i in range(10):
-        if letter =! 
+        if letter == vowels[i]:
+            counter += 1
+    if counter == 0:
+        print(letter)
+
+
+
 
 
 
