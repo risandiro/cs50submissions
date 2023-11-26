47c6f9f428d6270471c01971a377829342bb657e
risandiro <151243329+risandiro@users.noreply.github.com>
2023-11-26 20:51:12 +0000

    Sun, Nov 26, 2023, 9:51 PM +01:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,9 +1,6 @@
 input = input("Input: ")
 vowels = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"]
-output = ""
 
-for c in input:
-    if c != vowels[0]:
-        output += c
+print(vowel[0])
 
-print("Output:", output)
+
