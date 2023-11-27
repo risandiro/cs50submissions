55519c760076f9313cb0fc2b140bfd8df4dffc56
risandiro <151243329+risandiro@users.noreply.github.com>
2023-11-27 20:03:14 +0000

    Mon, Nov 27, 2023, 9:03 PM +01:00

--- plates/plates.py
+++ plates/plates.py
@@ -46,6 +46,8 @@ def rule_three(s):
 
     if counter <= 2:
         afd = s[counter:len(s)]
+        if s[counter] == 0:
+            return False
         if afd.isnumeric():
             return True
         else:
