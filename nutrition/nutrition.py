e8580dd08d971c16f08c98199cfc51f08ebfd1d9
risandiro <151243329+risandiro@users.noreply.github.com>
2023-11-29 18:13:51 +0000

    Wed, Nov 29, 2023, 7:13 PM +01:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,7 +1,7 @@
 fruit = input("Item: ")
 fruit = fruit.lower()
 
-dictionary = [
+dictionary = {
 {"apple": "130"},
 {"avocado": "50"},
 {"banana": "110"},
@@ -12,17 +12,15 @@ dictionary = [
 {"kiwifruit": "90"},
 {"lemon": "15"},
 {"lime": "20"},
-{"nectarine": "20"},
-{"apple": "130"},
-{"apple": "130"},
-{"apple": "130"},
-{"apple": "130"},
-{"apple": "130"},
-{"apple": "130"},
-{"apple": "130"},
-{"apple": "130"},
-{"apple": "130"},
-
-
-]
+{"nectarine": "60"},
+{"orange": "80"},
+{"peach": "60"},
+{"pear": "100"},
+{"pineapple": "50"},
+{"plums": "70"},
+{"strawberries": "50"},
+{"sweet cherries": "100"},
+{"tangarine": "50"},
+{"watermelon": "80"},
+}
 
