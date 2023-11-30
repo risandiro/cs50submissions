d04380894d393f74af29881811930c3543f0ce12
risandiro <151243329+risandiro@users.noreply.github.com>
2023-11-30 20:31:27 +0000

    Thu, Nov 30, 2023, 9:31 PM +01:00

--- number.py
+++ number.py
@@ -5,4 +5,9 @@ except ValueError:
 else:
         print(f"x is {x}")
 
+# without else function, x never gets defined if input is not an integer, because
+# the program is executed from right to left and when we get to the input conversion
+# into the integer, it creates a value error and jumps straight to exception without
+# ever getting to defining the x
+
 
