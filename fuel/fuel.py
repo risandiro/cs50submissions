8ce6d475bbd1ff9408a48c2e858b76f8932b3c30
risandiro <151243329+risandiro@users.noreply.github.com>
2023-11-30 21:45:25 +0000

    Thu, Nov 30, 2023, 10:45 PM +01:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -2,20 +2,18 @@ while True:
     try:
         x, y = input("Fraction: ").split("/")
         x , y = int(x), int(y)
-        percent = x / y * 100
-        if percent <= 1:
-            print("E")
-        elif percent <= 99:
-            print("F")
-        else:
-            print(f"{int(percent)}")
+        if y > x:
+            percent = x / y * 100
+            if percent <= 1:
+                print("E")
+            elif percent <= 99:
+                print("F")
+            else:
+                print(f"{int(percent)}")
 
-        break
+                break
 
     except (ValueError, ZeroDivisionError):
-        pass
-
-    except percent > 100:
         pass
 
 
