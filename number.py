3ccec85bd6897fa5deeb1974b05809a9edb3b8a9
risandiro <151243329+risandiro@users.noreply.github.com>
2023-11-30 20:35:50 +0000

    Thu, Nov 30, 2023, 9:35 PM +01:00

--- number.py
+++ number.py
@@ -1,14 +1,25 @@
-while True:
-    try:
-        x = int(input("What's x? "))
-    except ValueError:
-        print("x is not an integer")
-    else:
-            print(f"x is {x}")
+"""
+
+try:
+    x = int(input("What's x? "))
+except ValueError:
+    print("x is not an integer")
+else:
+    print(f"x is {x}")
 
 # without else function, x never gets defined if input is not an integer, because
 # the program is executed from right to left and when we get to the input conversion
 # into the integer, it creates a value error and jumps straight to exception without
 # ever getting to defining the x
 
+"""
 
+while True:
+    try:
+        x = int(input("What's x?"))
+    except ValueError:
+        print("x is not an integer")
+    else:
+        break
+
+print(f"x is {x}")
