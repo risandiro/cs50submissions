f26918dccbc9dbed08cacdef38d631f280f56723
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-01 18:07:58 +0000

    Fri, Dec 1, 2023, 7:07 PM +01:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -6,7 +6,7 @@ while True:
         if item in grocery_list:
             grocery_list[item] = grocery_list[item] + 1
         else:
-            grocery_list[item] = 0
+            grocery_list[item] = 1
     except EOFError:
         print(grocery_list)
         break
