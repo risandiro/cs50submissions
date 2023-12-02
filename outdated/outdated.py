498f233a4dc6314b00f6c0d89d27f5f16ecd167f
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-02 17:22:21 +0000

    Sat, Dec 2, 2023, 6:22 PM +01:00

---
+++ outdated/outdated.py
@@ -0,0 +1,18 @@
+months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+
+try: input("Date: ")
+
