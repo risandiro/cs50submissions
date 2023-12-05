c16beb1687851d33a66ba824b42ba06d3a3a2fbf
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-05 19:15:45 +0000

    Tue, Dec 5, 2023, 8:15 PM +01:00

--- itunes.py
+++ itunes.py
@@ -9,5 +9,6 @@ if len(sys.argv) != 2:
     sys.exit()
 
 response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
-print(json.dumps(response.json(), indent = 2))
-
+# print(json.dumps(response.json(), indent = 2))
+p = response.json()
+for 
