8b9a2777644e432ffed1fbe0b7480de466206428
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-05 20:22:01 +0000

    Tue, Dec 5, 2023, 9:22 PM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -4,9 +4,10 @@ import random
 from pyfiglet import Figlet
 
 fonts = figlet.getFonts()
+input = input("Input: ")
 
 if len(sys.argv) == 1:
-
+    figlet = Figlet(input)
 
 # if len(sys.argv == 3):
 
