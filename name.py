73df1cfc6deb59a6cc3b53969bfc44807305083e
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-05 18:16:54 +0000

    Tue, Dec 5, 2023, 7:16 PM +01:00

--- name.py
+++ name.py
@@ -10,4 +10,5 @@ else:
     # sys.argv[0] is "name.py"
     print("hello, my name is", sys.argv[1])
 
-# if the input is queted, python recognize it as a single item
+# if the input is queted, python recognize it as a single string, even
+# if there's a space between them
