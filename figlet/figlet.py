bea2e34a0b78a8b190a8380090228289113d828b
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-05 20:56:28 +0000

    Tue, Dec 5, 2023, 9:56 PM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -7,15 +7,13 @@ input = input("Input: ")
 fonts = Figlet()
 fonts = fonts.getFonts()
 shuffle(fonts)
-print(fonts)
 
-'''
+
 if len(sys.argv) == 1:
     x = Figlet()
     x.getFonts()
-    x.setFont(font="fonts")
+    x.setFont(font = fonts[1])
     print(x.renderText(input))
 
-elif len(sys.argv == 3):
-'''
+# elif len(sys.argv == 3):
 
