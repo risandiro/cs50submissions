97b9c21338792a00b479a96deb9a48ea070d9470
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-05 17:14:46 +0000

    Tue, Dec 5, 2023, 6:14 PM +01:00

--- generate.py
+++ generate.py
@@ -1,10 +1,15 @@
 # import random
-from random import choice, randint
+from random import choice, randint, shuffle
 
 # random.choice -> choice
 coin = choice(["heads", "tails"])
 # print(coin)
 
 number = randint(1, 10)
-print(number)
+# print(number)
 
+# it doesn't just return a value, it shuffles the whole list
+cards = ["jack", "queen", "king"]
+shuffle(cards)
+for i in cards:
+    print()
