cc520882572c5bb41ab40adcaa17896d53b209fb
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-05 18:26:47 +0000

    Tue, Dec 5, 2023, 7:26 PM +01:00

--- name.py
+++ name.py
@@ -1,10 +1,14 @@
+'''
+
 #   docs.python.org/3/library/sys.html
 import sys
 
 if len(sys.argv) < 2:
     # len 1 is the name of the program, len 2 is the expected input
     sys.exit("Too few arguments")
-elif len(sys.argv) > 2:
+
+if len(sys.argv) > 2:
+    # len 1 is the name of the program, len 2 is the expected input
     sys.exit("Too many arguments")
 
 # sys.argv[0] is "name.py"
@@ -12,3 +16,12 @@ print("hello, my name is", sys.argv[1])
 
 # if a input is queted, it's recognized as a single string, even if there's a space between them
 
+'''
+
+import sys
+
+if len(sys.argv) < 2:
+    sys.exit("Too few arguments")
+
+for arg in sys.argv:
+    print("hello, my name is", arg)
