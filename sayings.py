3fbd8cba23a2ef08116dd92ce2d429c01cecb7c0
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-05 19:47:04 +0000

    Tue, Dec 5, 2023, 8:47 PM +01:00

--- sayings.py
+++ sayings.py
@@ -8,9 +8,10 @@ def hello(name):
 def goodbye(name):
     print(f"goodbye, {name}")
 
+
 # main will not get called if we are importing the file, let's say because we want to
 # use those defined functions in a different code
+
+# if the name of this file is the same as name of the file, where it's defined, run main
 if __name__ == "__main__":
     main()
-
-# if the name of this directory is the same as name of the directory, where it's called, run main
--- sayings2.py
+++ sayings2.py
@@ -1,6 +1,7 @@
 import sys
 
-from sayings import hello
+from sayings import hello, goodbye
 
 if len(sys.argv) == 2:
-    hello(sys.argv[1])
+    hello(sys.argv[0])
+    goodbye(sys.argv[1])
