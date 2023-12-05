a8e475eeee27ebdb0aa3701708b6fed8bf4b3174
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-05 18:11:57 +0000

    Tue, Dec 5, 2023, 7:11 PM +01:00

--- name.py
+++ name.py
@@ -1,8 +1,11 @@
 #   docs.python.org/3/library/sys.html
 import sys
 
-try:
+if len(sys.argv) < 2:
+    # len 1 is the name of the program, len 2 is the expected input
+    print("Too few arguments")
+elif len(sys.argv) > 2:
+    print("Too many arguments")
+else:
     # sys.argv[0] is "name.py"
     print("hello, my name is", sys.argv[1])
-except IndexError:
-    print("Too few arguments")
