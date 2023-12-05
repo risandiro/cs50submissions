7426774683202231d5ea8419acd4898031192299
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-05 19:18:28 +0000

    Tue, Dec 5, 2023, 8:18 PM +01:00

--- itunes.py
+++ itunes.py
@@ -9,6 +9,5 @@ if len(sys.argv) != 2:
     sys.exit()
 
 response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
-# print(response.json)
-# print(json.dumps(response.json(), indent = 2))
-print(response.json)
+# print(response.json())  -> full json response
+# print(json.dumps(response.json(), indent = 2))  -> more readable json response
