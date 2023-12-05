353b3308592152e3cfe98a90b92d91d37e1d14a4
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-05 18:34:28 +0000

    Tue, Dec 5, 2023, 7:34 PM +01:00

--- name.py
+++ name.py
@@ -24,5 +24,6 @@ if len(sys.argv) < 2:
     sys.exit("Too few arguments")
 
 # outputs a slice of the list, starting from 1 to the end
-for arg in sys.argv[1:]:
+# using a negative number slices the list from the other side
+for arg in sys.argv[1:-1]:
     print("hello, my name is", arg)
