523d6870a30c5b59845419c93c524b32fc43ce23
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-05 18:21:30 +0000

    Tue, Dec 5, 2023, 7:21 PM +01:00

--- name.py
+++ name.py
@@ -3,12 +3,12 @@ import sys
 
 if len(sys.argv) < 2:
     # len 1 is the name of the program, len 2 is the expected input
-    print("Too few arguments")
+    sys.exit("Too few arguments")
 elif len(sys.argv) > 2:
-    print("Too many arguments")
-else:
-    # sys.argv[0] is "name.py"
-    print("hello, my name is", sys.argv[1])
+    sys.exit("Too many arguments")
+
+# sys.argv[0] is "name.py"
+print("hello, my name is", sys.argv[1])
 
 # if a input is queted, it's recognized as a single string, even if there's a space between them
 
