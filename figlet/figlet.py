cdd5e2fa6e97b51a4a74e63b3577fa12a1445738
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-05 20:31:00 +0000

    Tue, Dec 5, 2023, 9:31 PM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -5,7 +5,8 @@ from pyfiglet import Figlet
 figlet = Figlet()
 fonts = figlet.getFonts()
 input = input("Input: ")
-
+figlet = figlet.setFont(font="coinstak")
+print(figlet)
 
 # if len(sys.argv == 1):
 
