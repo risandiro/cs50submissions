f34f2a0e10c6bbcd4f5985c066e6dcb480cbd8e2
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-05 19:44:23 +0000

    Tue, Dec 5, 2023, 8:44 PM +01:00

--- sayings.py
+++ sayings.py
@@ -8,5 +8,9 @@ def hello(name):
 def goodbye(name):
     print(f"goodbye, {name}")
 
+# main will not get called if we are importing the file, let's say because we want to
+# use those defined functions in a different code
 if __name__ == "__main__":
     main()
+
+# if the name of this directory is the same as name of the directory, where it's called, run main
