def4554d9f40fd544ce368cdebe0efe4e848295f
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-06 18:39:01 +0000

    Wed, Dec 6, 2023, 7:39 PM +01:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -8,7 +8,10 @@ if len(sys.argv) == 2:
         nmbr = float(sys.argv[1])
         response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
         response = response.json()
-        
+
 
     except ValueError:
         sys.exit("Command-line argument is not a number")
+
+    except requests.RequestsException:
+        pass
