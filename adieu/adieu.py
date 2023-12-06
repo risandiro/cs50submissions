245ff93184bd710f3ea0483f53add6ca59e2a737
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-06 15:47:36 +0000

    Wed, Dec 6, 2023, 4:47 PM +01:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -5,4 +5,7 @@ while True:
         name = input("Name: ")
         name_list.append(name)
     except EOFError:
-        if len(name_list)
+        if len(name_list) == 1:
+            print("Adieu, adieu, to", name_list[0])
+        if len(name_list) == 2:
+            print(f"Adieu, adieu, to {name_list[0]} and {name_list[1]}")
