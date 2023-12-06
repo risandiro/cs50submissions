3ec143246a559f5dbf412f31603859b9604df9ba
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-06 18:32:45 +0000

    Wed, Dec 6, 2023, 7:32 PM +01:00

---
+++ bitcoin/bitcoin.py
@@ -0,0 +1,10 @@
+import sys, requests
+
+if len(sys.argv) == 1:
+    print("Missing command-line argument")
+
+if len(sys.argv) == 2:
+    try:
+        nmbr = float(sys.argv[1])
+    except ValueError:
+        
