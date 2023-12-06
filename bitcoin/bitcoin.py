f2a4a097aca0e9b93453e6f3d1f5fde33f25a650
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-06 18:35:27 +0000

    Wed, Dec 6, 2023, 7:35 PM +01:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -6,5 +6,8 @@ if len(sys.argv) == 1:
 if len(sys.argv) == 2:
     try:
         nmbr = float(sys.argv[1])
+
+
+
     except ValueError:
-        print("Command-line argument is not a number")
+        sys.exit("Command-line argument is not a number")
