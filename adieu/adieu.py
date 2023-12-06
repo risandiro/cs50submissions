537d23d736378466c66ef121fdd9a5597b7b588a
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-06 15:53:12 +0000

    Wed, Dec 6, 2023, 4:53 PM +01:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -12,3 +12,8 @@ while True:
         elif len(name_list) == 2:
             print(f"Adieu, adieu, to {name_list[0]} and {name_list[1]}")
             break
+
+        elif len(name_list) > 2:
+            print("Adieu, adieu, to ", sep="", end="")
+            for person in name_list:
+                print(person, )
