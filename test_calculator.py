0ba77de5b145dd359e0bb2b4678f5e512ab1ffa3
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-07 17:10:58 +0000

    Thu, Dec 7, 2023, 6:10 PM +01:00

--- test_calculator.py
+++ test_calculator.py
@@ -1,5 +1,17 @@
 from calculator import square
 
+def main():
+    test_square()
+
+
 
 def test_square():
-    if 
+    if square(2) != 4:
+        print("2 was not 4")
+    if square(3) != 9:
+        print("3 was not 9")
+
+
+
+if __name__ == "__main__":
+    main()
