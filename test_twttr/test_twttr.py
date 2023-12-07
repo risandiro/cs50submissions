dfd2213a7dd5480ddf083f2d7ce492134e5cfd08
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-07 18:53:23 +0000

    Thu, Dec 7, 2023, 7:53 PM +01:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -1,7 +1,4 @@
 from twttr import shorten
 
 def test_string():
-    assert shorten("miazga") == "mzg"
-
-def test_integer():
-    assert shorten(int(1)) == int(1)
+    assert shorten("miazga") == "Output: mzg"
