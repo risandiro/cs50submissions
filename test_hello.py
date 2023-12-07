db0c6ac1f8b7c496315071f448a26054745dd66a
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-07 18:12:19 +0000

    Thu, Dec 7, 2023, 7:12 PM +01:00

--- test_hello.py
+++ test_hello.py
@@ -4,5 +4,7 @@ def test_default():
     assert hello() == "hello, world"
 
 def test_argument():
-    assert hello("David") == "hello, David"
+    #assert hello("David") == "hello, David"
+    for name in ["David", "Mike", "John"]:
+        assert hello(name) == f"hello, {name}"
 
