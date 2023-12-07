a56c7ea9e4e4f5299515d6bd05efd33d22c3c1b7
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-07 18:55:52 +0000

    Thu, Dec 7, 2023, 7:55 PM +01:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -1,4 +1,9 @@
 from twttr import shorten
 
-def test_string():
+def test_lowercase():
     assert shorten("miazga") == "Output: mzg"
+    assert shorten("krk") == "Output: krk"
+
+def test_uppercase():
+    assert shorten("MIAZGA") == "Output: MZG"
+    assert shorten("KRK") == "Output: KRK"
