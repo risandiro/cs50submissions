a330d4509062aadaa3a96d240763f0f692bfee46
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-07 23:02:07 +0000

    Fri, Dec 8, 2023, 12:02 AM +01:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -1,5 +1,9 @@
 from twttr import shorten
 
+def main()
+    test_lowercase()
+    test_uppercase()
+    test_integer()
 
 def test_lowercase():
     assert shorten("miazga") == "Output: mzg"
@@ -12,3 +16,5 @@ def test_uppercase():
 def test_integer():
     assert shorten(int(5)) == "Output: 5"
 
+if __name__ == "__main__":
+    main()
