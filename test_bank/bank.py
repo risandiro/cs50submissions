5277865fe88d31678068a1f6dab4007fa3026d35
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-09 19:31:29 +0000

    Sat, Dec 9, 2023, 8:31 PM +01:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -3,12 +3,13 @@ def main():
     x = x.strip()
 
 def value(greeting):
-    if greeting.startswith("hello") or x.startswith("Hello") == True:
-        print("$0")
-    elif x.startswith("h") or x.startswith("H") == True:
-        print("$20")
+    if greeting.startswith("hello") or greeting.startswith("Hello"):
+        return "$0"
+    elif greeting.startswith("h") or greeting.startswith("H"):
+        return "$20"
     else:
-        print("$100")
+        return "$100"
 
-
+if __name__ == "__main__":
+    main()
 
