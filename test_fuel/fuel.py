9cef46dd71eeac7bb706238dc840ba8f79f61da1
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-09 21:12:18 +0000

    Sat, Dec 9, 2023, 10:12 PM +01:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -3,21 +3,21 @@ def main():
 
 
 def convert(fraction):
-while True:
-    try:
-        x, y = input("Fraction: ").split("/")
-        x , y = int(x), int(y)
-        if y >= x:
-            percent = int(round(x / y * 100))
-            if percent <= 1:
-                print("E")
-                break
-            elif percent >= 99:
-                print("F")
-                break
-            else:
-                print(f"{percent}%")
-                break
+    while True:
+        try:
+            x, y = fraction.split("/")
+            x , y = int(x), int(y)
+            if y >= x:
+                percent = int(round(x / y * 100))
+                if percent <= 1:
+                    print("E")
+                    break
+                elif percent >= 99:
+                    print("F")
+                    break
+                else:
+                    print(f"{percent}%")
+                    break
 
-    except (ValueError, ZeroDivisionError):
+        except (ValueError, ZeroDivisionError):
         pass
