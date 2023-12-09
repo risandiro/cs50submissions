ef81f57e440128c2a2991e8836187e2f18df24ff
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-09 21:34:25 +0000

    Sat, Dec 9, 2023, 10:34 PM +01:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -2,6 +2,19 @@ from fuel import convert, gauge
 import pytest
 
 
-def test_empty():
+def test_x_is_greater_than_y():
     with pytest.raises(ValueError):
         convert("4/3")
+
+def test_x_is_not_an_integer():
+    with pytest.raises(ValueError):
+        convert("x/3")
+
+def test_x_is_not_an_integer():
+    with pytest.raises(ValueError):
+        convert("3/x")
+
+def test_zero_division():
+    with pytest.raises(ZeroDivisionError):
+        convert("3/0")
+
