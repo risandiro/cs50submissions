9073816b5e41cfc9c0ed523a85fa0929aa1413c3
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-09 20:38:54 +0000

    Sat, Dec 9, 2023, 9:38 PM +01:00

--- test_plates/plates.py
+++ test_plates/plates.py
@@ -41,7 +41,7 @@ def rule_two(s):
 def rule_three(s):
     counter = 0
     for char in range(len(s)):
-        while counter < 1:
+        if counter < 1:
             if s[char].isnumeric():
                 counter += 1
                 continue
