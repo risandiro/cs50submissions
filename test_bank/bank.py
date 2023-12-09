7b3a357524c726145c59dce6a7607dad7d0075d1
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-09 19:28:58 +0000

    Sat, Dec 9, 2023, 8:28 PM +01:00

---
+++ test_bank/bank.py
@@ -0,0 +1,14 @@
+def main():
+    x = input("Greeting: ")
+    x = x.strip()
+
+def value(greeting):
+    if greeting.startswith("hello") or x.startswith("Hello") == True:
+        print("$0")
+    elif x.startswith("h") or x.startswith("H") == True:
+        print("$20")
+    else:
+        print("$100")
+
+
+
