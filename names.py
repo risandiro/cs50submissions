302f570ad9123623af7394242129edd1657d2016
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-09 23:07:20 +0000

    Sun, Dec 10, 2023, 12:07 AM +01:00

--- names.py
+++ names.py
@@ -12,7 +12,6 @@ name = input("What's your name? ")
 # if the file doesn't exist, python creates it
 # next we specify what we want to do with it "w", means write and allows to change the content
 file = open("names.txt", "w")
-
 file.write(name)
 file.close()
 
