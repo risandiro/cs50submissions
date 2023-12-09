51146daf8d83f5a14584119e5bd4749d0f62c6e8
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-09 21:37:35 +0000

    Sat, Dec 9, 2023, 10:37 PM +01:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -11,6 +11,8 @@ def main():
 def convert(fraction):
     x, y = fraction.split("/")
     x , y = int(x), int(y)
+    if y == 0:
+        raise ZeroDivisionError
     if y >= x:
         return int(round(x / y * 100))
     else:
