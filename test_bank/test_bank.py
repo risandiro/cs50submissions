2344c15892f30d82e73fc9ab12832b6f891b46df
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-09 19:42:58 +0000

    Sat, Dec 9, 2023, 8:42 PM +01:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -1,1 +1,16 @@
 from bank import value
+
+def test_0():
+    assert value("hello") == 0
+    assert value("Hello") == 0
+    assert value("hElLo") == 0
+
+def test_20():
+    assert value("howdy sailor") == 20
+    assert value("HOWDY SAILOR") == 20
+    assert value("hOwDy_sAiLoR") == 20
+
+def test_100():
+    assert value("abrakadabra") == 100
+    assert value("Chuppa Chupps") == 100
+    assert value() == 100
