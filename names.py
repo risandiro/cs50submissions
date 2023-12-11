ba6eed028829e1145440baeb5f0094f27185aa22
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-11 11:16:01 +0000

    Mon, Dec 11, 2023, 12:16 PM +01:00

--- names.py
+++ names.py
@@ -109,3 +109,6 @@ with open("students.csv") as file:
     reader = csv.DictReader(file)
     for row in reader:
         students.append({"name": row["name"], "house": row["house"], "home": row["home"]})
+
+    for student in sorted(students, key=lambda student: student["name"])
+        print(student["house"])
