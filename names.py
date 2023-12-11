8f8464d1d4af1d8532b9c417206915d6cef1a5c5
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-11 15:39:11 +0000

    Mon, Dec 11, 2023, 4:39 PM +01:00

--- names.py
+++ names.py
@@ -140,7 +140,3 @@ with open("students.csv", "a") as file:
 with open("students.csv", "a") as file:
     writer = csv.DictWriter(file, fieldnames=["name", "home"])
     writer.writerow({"name": name, "home": home})
-
-# -------------------------------------------
-
-
