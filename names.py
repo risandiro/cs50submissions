38c2964d17fe2b8b0157768217c86be116ad9333
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-11 10:56:51 +0000

    Mon, Dec 11, 2023, 11:56 AM +01:00

--- names.py
+++ names.py
@@ -86,3 +86,12 @@ for student in sorted(students, key=get_house):
 # we replace name with lambda, argument: return value
 for student in sorted(students, key=lambda student: student["name"])
     print(f"{student['name']} is in {student['house']}")
+
+# ------------------------------------------
+
+import csv
+
+students = []
+with open("students.csv") as file:
+    reader = csv.reader(file)
+    for row in reader:
