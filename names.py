a53edeba622341232eb722293e7ade628063a849
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-11 00:22:41 +0000

    Mon, Dec 11, 2023, 1:22 AM +01:00

--- names.py
+++ names.py
@@ -69,6 +69,9 @@ with open("names.csv") as file:
         student = {"name": name, "house": house}
         students.append(student)
 
-for student in students:
+def get_name(student):
+    return student["name"]
+
+for student in sorted(students, key=get_name):
     print(f"{student['name']} is in {student['house']}")
 
