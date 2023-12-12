9cc6d5c875d84104a4ce2ac575b77459500b020a
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-12 14:03:40 +0000

    Tue, Dec 12, 2023, 3:03 PM +01:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,5 +1,17 @@
 import sys
 
-if len(sys.argv) == 2:
+def main():
 
-    
+
+
+def cma_check():
+    if len(sys.argv) == 2:
+        return True
+    elif len(sys.argv) < 2:
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 2:
+        sys.exit("Too many command-line arguments")
+
+
+if __name__ == "__main__":
+    main()
