09c22603a4f78ff505c5aabda1a5e61a36fd3c55
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-12 14:27:01 +0000

    Tue, Dec 12, 2023, 3:27 PM +01:00

---
+++ lines/goodbye.py
@@ -0,0 +1,17 @@
+'''
+
+Say goodbye
+
+'''
+
+names =[]
+
+for i in range(2):
+    names.append(input("What's your name? "))
+
+
+for name in names:
+    print(f"goodbye,", name)
+
+""" Print goodybe """
+
