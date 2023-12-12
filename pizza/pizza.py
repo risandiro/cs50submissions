91ab7ee3c159e3b588cb1447444ca0a9d133ab9e
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-12 19:46:23 +0000

    Tue, Dec 12, 2023, 8:46 PM +01:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -1,1 +1,25 @@
 import tabulate
+
+def main():
+    cma_check()
+    file_check()
+
+
+
+def cma_check():
+    if len(sys.argv) == 2:
+        return
+    elif len(sys.argv) < 2:
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 2:
+        sys.exit("Too many command-line arguments")
+
+def file_check():
+    if sys.argv[1].endswith(".csv"):
+        return
+    else:
+        sys.exit("Not a Python file")
+
+
+if __name__ == "__main__":
+    main()
