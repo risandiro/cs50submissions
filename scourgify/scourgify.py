0064e9a20ae318d5ae21b26f5321cd67dad1d787
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-14 16:25:07 +0000

    Thu, Dec 14, 2023, 5:25 PM +01:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -2,19 +2,19 @@ import sys, csv
 
 def main():
     cma_check()
-    data = []
     try:
-
+        data = []
         with open(sys.argv[1]) as file:
             reader = csv.DictReader(file)
             for row in reader:
-                first_name, last_name = row["name"].strip().split(",")
+                last_name, first_name = row["name"].strip().split(",")
                 data.append({"first name": first_name, "last name": last_name, "house": row["house"]})
-                print(data)
 
 
     except FileNotFoundError:
         sys.exit(f"Could not read {sys.argv[1]}")
+
+
 
 
 def cma_check():
