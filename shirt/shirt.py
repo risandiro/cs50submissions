f63c4f278099e7ab39fc7defb351a5bfb6d98829
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-14 17:41:06 +0000

    Thu, Dec 14, 2023, 6:41 PM +01:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -1,8 +1,19 @@
+import sys
+
 def main():
 
 
 def input_check():
-    
+    if len(sys.argv) == 3:
+        if sys.argv[1].endswith(".jpg") and sys.argv[2]
+
+
+    elif len(sys.argv) < 3:
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 3:
+        sys.exit("Too many command-line arguments")
+
+
 
 
 if __name__ == "__main__":
