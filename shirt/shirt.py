d8f7b8f9e438a134ae0d8666f8525dff1b1dfcde
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-14 22:57:29 +0000

    Thu, Dec 14, 2023, 11:57 PM +01:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -1,17 +1,19 @@
 import sys
 
 def main():
+    input_check()
+
 
 
 def input_check():
     if len(sys.argv) == 3:
-        if sys.argv[1].endswith([".jpg", ".jpeg", ".png"])
-            if sys.argv[2].endswith([".jpg", ".jpeg", ".png"])
+        if sys.argv[1].endswith([".jpg", ".jpeg", ".png"]):
+            if sys.argv[2].endswith([".jpg", ".jpeg", ".png"]):
                 _, ext1 = sys.argv[1].split(".")
                 _, ext2 = sys.argv[2].split(".")
                 if ext1 == ext2:
                     return
-                
+
 
 
             else:
