5d3db81c3d8edc41c2e78b86fac0e1670cd766b0
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-14 15:15:27 +0000

    Thu, Dec 14, 2023, 4:15 PM +01:00

---
+++ scourgify/scourgify.py
@@ -0,0 +1,17 @@
+import sys
+
+def main():
+    cma_check()
+
+
+def cma_check():
+    if sys.argv == 3:
+        return
+    elif sys.argv < 3:
+        sys.exit("Too few command-line arguments")
+    elif sys.argv > 3:
+        sys.exit("Too many command-line arguments")
+
+
+if __name__ == __"main"__:
+    main()
