696a5167cdbc7a160aafe63522edc0af0785c5da
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-14 17:49:25 +0000

    Thu, Dec 14, 2023, 6:49 PM +01:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -5,12 +5,19 @@ def main():
 
 def input_check():
     if len(sys.argv) == 3:
-        if sys.argv[1].endswith([".jpg", "jpeg", "png"])
-            if sys.argv[2].endswith([".jpg", "jpeg", "png"])
+        if sys.argv[1].endswith([".jpg", ".jpeg", ".png"])
+            if sys.argv[2].endswith([".jpg", ".jpeg", ".png"])
+                _, ext1 = sys.argv[1].split(".")
+                _, ext2 = sys.argv[2].split(".")
+                if ext1 == ext2:
+                    return
+                
 
 
+            else:
+                sys.exit("Invalid output")
         else:
-            
+            sys.exit("Invalid input")
     elif len(sys.argv) < 3:
         sys.exit("Too few command-line arguments")
     elif len(sys.argv) > 3:
