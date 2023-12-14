c759aeb5bb4c5c7021f422da2e8c4c485a2084c6
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-14 16:48:45 +0000

    Thu, Dec 14, 2023, 5:48 PM +01:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -12,7 +12,6 @@ def main():
                 for row in reader:
                     last_name, first_name = row["name"].strip().split(",")
                     with open(sys.argv[2], "a") as file_1:
-                            writer = csv.DictWriter(file_1)
                             writer.writerow({"first name": first_name, "last name": last_name, "house": row["house"]})
 
     except FileNotFoundError:
