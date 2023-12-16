35d6f0bd8cd6d112853823f1962719feb447da60
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-16 21:58:33 +0000

    Sat, Dec 16, 2023, 10:58 PM +01:00

--- validate.py
+++ validate.py
@@ -1,7 +1,3 @@
  email = input("What's your email? ").strip()
 
-# if this string1 and string2 is somewhere in another string, no matter where it is, return True
-if "@" in email and "." in email:
-    print("Valid")
-else:
-   print("Invalid")
+username, domain = email.split("@")
