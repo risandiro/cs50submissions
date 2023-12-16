383ae25939fff6a43b57f54c6853055aa2b60211
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-16 23:02:58 +0000

    Sun, Dec 17, 2023, 12:02 AM +01:00

--- validate.py
+++ validate.py
@@ -17,7 +17,10 @@ import re
    {m,n}  m-n repetitions
 
     ^     matches the start of the string
-    $     matches the end of the string just before the newline at the end of the string '''
+    $     matches the end of the string just before the newline at the end of the string
+    
+    []    set of characters
+    [^]   complementing the set '''
 
 # backslash before means literally a dot, not a dot from the list above
 # r" or raw string means to not interpret any backslashes as a escape sequance
