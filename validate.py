784b4aff3bd653e88f31873cd011f5544bc3751e
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-16 21:57:00 +0000

    Sat, Dec 16, 2023, 10:57 PM +01:00

--- validate.py
+++ validate.py
@@ -1,7 +1,7 @@
  email = input("What's your email? ").strip()
 
-# if this string is somewhere in another string, no matter where it is, return True
-if "@" in email:
+# if this string1 and string2 is somewhere in another string, no matter where it is, return True
+if "@" in email and "." in email:
     print("Valid")
 else:
    print("Invalid")
