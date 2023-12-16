c30ec5d98ff9dc7ba3cd3f2a27a327274e90657e
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-16 22:19:27 +0000

    Sat, Dec 16, 2023, 11:19 PM +01:00

--- validate.py
+++ validate.py
@@ -7,4 +7,6 @@ username, domain = email.split("@")
 # if username: (True if username contains anything except None and "")
 # if username and "." in domain: --> means --> if (username) and ("." in domain):
 
+import re
 
+re.search
