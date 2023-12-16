4d8c0fd29a0d8e7f886304e8836ed569af998158
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-16 21:38:26 +0000

    Sat, Dec 16, 2023, 10:38 PM +01:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -9,7 +9,7 @@ def main():
         size = shirt.size
 
         image = ImageOps.fit(image, size)
-        image.paste(image, mask=shirt)
+        image.paste(image, shirt)
 
         image.save(sys.argv[2])
 
