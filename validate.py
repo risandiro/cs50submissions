33fab50513619a911147d661d15e1c661b961581
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-16 22:25:49 +0000

    Sat, Dec 16, 2023, 11:25 PM +01:00

--- validate.py
+++ validate.py
@@ -8,5 +8,13 @@ username, domain = email.split("@")
 # if username and "." in domain: --> means --> if (username) and ("." in domain):
 
 import re
+
+''' .     any character except a newline
+    *     0 or more repetitions
+    +     1 or more repetitions
+    ?     0 or 1 repetition
+   {m}    m repetitions
+   {m,n}  m-n repetitions '''
+
 # re.search(pattern #string)
 re.search("@", email):
