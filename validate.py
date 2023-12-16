d5a843dd231174892054bfe6cc80f955c14cf081
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-16 22:47:55 +0000

    Sat, Dec 16, 2023, 11:47 PM +01:00

--- validate.py
+++ validate.py
@@ -17,6 +17,6 @@ import re
    {m,n}  m-n repetitions '''
 
 # backslash before means literally a dot, not a dot from the list above
-# r" string means to not interpret any backslashes as a escape sequance
+# r" or raw string means to not interpret any backslashes as a escape sequance
 # re.search(pattern #string)
 re.search(r"..*@.+\.edu ", email):
