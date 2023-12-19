77a1b8e46e943d83464fbe797b4fc88ccec51921
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-19 23:07:02 +0000

    Wed, Dec 20, 2023, 12:07 AM +01:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -7,11 +7,12 @@ def main():
 def validate(ip):
     try:
         a, b, c, d = ip.split(".")
-        return True
-
+        a, b, c, d = int(a), int(b), int(c), int(d)
+        if 0 >= a <= 255 and 0 >= b <= 255 and 0 >= c <= 255 and 0 >= d <= 255:
+            return True
+        return False
     except ValueError:
        return False
-
 
 
 if __name__ == "__main__":
