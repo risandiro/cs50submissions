85dcb35a81b474495e7ef0cb81a9cdddb8f0f339
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-19 19:08:33 +0000

    Tue, Dec 19, 2023, 8:08 PM +01:00

--- twitter.py
+++ twitter.py
@@ -19,4 +19,5 @@ print(username)
 
 url = input("URL: ").strip()
 
-if matches := re.search(r"https?://(www\.)?twitter\.com/(.+)$", url, re.IGNORECASE)
+if matches := re.search(r"(?:https?://)?(?:www\.)?twitter\.com/(.+)$", url, re.IGNORECASE)
+    print(f"Username:", matches.group(1))
