0ae754825959409cebab8e1e25a516895571d0f2
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-19 18:12:16 +0000

    Tue, Dec 19, 2023, 7:12 PM +01:00

--- format.py
+++ format.py
@@ -2,8 +2,9 @@ import re
 
 name = input("What's your name? ").strip()
 
+# walrus operator if you want to first assign a value and then use it in boolean expression
 # using brackets to capture the outputting values
-if matches = re.search(r"^(.+), *(.+)$", name):
+if matches := re.search(r"^(.+), *(.+)$", name):
     # last, first = matches.groups()
     name = matches.group(2) + " " + matches.group(1)
 
