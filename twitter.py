bfd1b0c020499c2db3bbac904133773c0e041fd4
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-19 18:29:43 +0000

    Tue, Dec 19, 2023, 7:29 PM +01:00

--- twitter.py
+++ twitter.py
@@ -1,5 +1,8 @@
 url = input("URL: ").strip()
 
 # string.replace(find, replace)
-username = url.replace("https://twitter.com/", "")
+# username = url.replace("https://twitter.com/", "")
+
+# string.removeprefix(remove a part of the string and everything before it)
+username = url.removeprefix("https://twitter.com/")
 print(username)
