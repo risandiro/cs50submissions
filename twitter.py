f831ef3705574fe023426219cb2d254063060c20
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-19 18:48:38 +0000

    Tue, Dec 19, 2023, 7:48 PM +01:00

--- twitter.py
+++ twitter.py
@@ -8,10 +8,9 @@ username = url.replace("https://twitter.com/", "")
 # string.removeprefix(removes a prefix but leaves alone if it doesn't start with prefix)
 username = url.removeprefix("https://twitter.com/")
 
+
 # re.sub(pattern, repl, string, count=0, flags=0)
 
-
-
-
+username = re.sub(r"https://twitter.com/", "", url)
 
 print(username)
