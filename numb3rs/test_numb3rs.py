6fba33236d6a8c468fe88207d8b14f49afea1e5e
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-19 23:24:35 +0000

    Wed, Dec 20, 2023, 12:24 AM +01:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -5,10 +5,10 @@ def test_validate_range():
     assert validate("256.0.0.254") == False
     assert validate("-1.5.85.47") == False
     assert validate("1.2.3.4") == True
+    assert validate("255.41.999.47") == False
 
 def test_validate_input():
     assert validate(".1.2.3.4") == False
     assert validate("255.255.45") == False
     assert validate("....") == False
     assert validate("7.7.7.7.7") == False
-    
