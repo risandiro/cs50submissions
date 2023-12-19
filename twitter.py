93295626bbfd97295fb860080d6626ec3c69bc62
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-19 18:34:15 +0000

    Tue, Dec 19, 2023, 7:34 PM +01:00

--- twitter.py
+++ twitter.py
@@ -1,8 +1,17 @@
+import re
+
 url = input("URL: ").strip()
 
 # string.replace(find, replace)
-# username = url.replace("https://twitter.com/", "")
+username = url.replace("https://twitter.com/", "")
 
-# string.removeprefix(remove a part of the string and everything before it)
+# string.removeprefix(removes a prefix but leaves alone if it doesn't start with prefix)
 username = url.removeprefix("https://twitter.com/")
+
+re.sub(pattern, repl, string, count=0, flags=0)
+
+
+
+
+
 print(username)
