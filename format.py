3fb750c4a58c2b9d9f8d7a05cbe15c0406fe5d06
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-19 18:03:17 +0000

    Tue, Dec 19, 2023, 7:03 PM +01:00

--- format.py
+++ format.py
@@ -5,7 +5,10 @@ name = input("What's your name? ").strip()
 # using brackets to capture the outputting values
 matches = re.search(r"^(.+),(.+)$", name)
 if matches:
-    last, first = matches.groups()
+    # last, first = matches.groups()
+    last = matches.group(1)
+    first = matches.group(2)
+
     name = f"{first} {last}"
 
 print(f"hello, {name}")
