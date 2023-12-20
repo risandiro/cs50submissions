0ba11557dce5d688df77f6ee66b756343f330e39
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-20 18:54:01 +0000

    Wed, Dec 20, 2023, 7:54 PM +01:00

--- working/working.py
+++ working/working.py
@@ -7,8 +7,9 @@ def main():
 def convert(s):
     if matches := re.search(r"^(([1-9]|10|11)(:[0-5][0-9])?|12(:00)?) (AM|PM) to (([1-9]|10|11)(:[0-5][0-9])?|12(:00)?) (AM|PM)", s):
         first_number = matches.group(1)
-        second_number = matches.group(4)
-        print(first_number, second_number)
+        first_ampm = matches.group(5)
+        second_number = matches.group(6)
+        print(first_number, second_number, first_ampm, sep="\n")
         return True
     else:
         raise ValueError
