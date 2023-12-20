34c3fd1dd3137d149fcb689c100a5ebb97c1830c
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-20 16:12:44 +0000

    Wed, Dec 20, 2023, 5:12 PM +01:00

--- working/working.py
+++ working/working.py
@@ -5,7 +5,7 @@ def main():
     print(convert(input("Hours: ").strip()))
 
 def convert(s):
-    if matches := re.search(r"^([0-9]|(10|11|12)) (:[0-6][0-9])? (AM|PM) to ([0-9]|(10|11|12)) (AM|PM)$", s):
+    if matches := re.search(r"^([0-9]|(10|11|12))(:[0-6][0-9])? (AM|PM) to ([0-9]|(10|11|12))(:[0-6][0-9])? (AM|PM)$", s):
         return True
     return False
 
