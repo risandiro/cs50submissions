dedf8d732dfaf073f9a43df5a7fd23f80f3f7ff4
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-20 18:57:24 +0000

    Wed, Dec 20, 2023, 7:57 PM +01:00

--- working/working.py
+++ working/working.py
@@ -5,12 +5,12 @@ def main():
     print(convert(input("Hours: ").strip()))
 
 def convert(s):
-    if matches := re.search(r"^((?:[1-9]|10|11)(?::[0-5][0-9])?|12(:00)?) (AM|PM) to ((?:[1-9]|10|11)(:[0-5][0-9])?|12(:00)?) (AM|PM)", s):
+    if matches := re.search(r"^((?:[1-9]|10|11)(?::[0-5][0-9])?|12(?::00)?) (AM|PM) to ((?:[1-9]|10|11)(?::[0-5][0-9])?|12(?::00)?) (AM|PM)", s):
         first_number = matches.group(1)
         first_ampm = matches.group(2)
         second_number = matches.group(3)
         second_ampm = matches.group(4)
-        print(first_number, second_number, first_ampm, sep="\n")
+        print(first_number, second_number, first_ampm, second_ampm, sep="\n")
         return True
     else:
         raise ValueError
