08984f2c64e9edc790a7d85e67eb159fdf94a06c
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-20 15:13:48 +0000

    Wed, Dec 20, 2023, 4:13 PM +01:00

--- watch/watch.py
+++ watch/watch.py
@@ -6,8 +6,9 @@ def main():
 
 
 def parse(s):
-    if matches := re.search(r"^<iframe .+ src=\"https?://(?:www.)?youtube.com/embed/[a-zA-Z0-9]+\"></iframe>$", s)
-
+    if matches := re.search(r"^<iframe .+ src=\"https?://(?:www.)?youtube.com/embed/[a-zA-Z0-9]+\"></iframe>$", s):
+        return True
+    return False
 
 if __name__ == "__main__":
     main()
