8bc2563a022f3dabc94fba3272e5a19981855e3e
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-20 15:37:20 +0000

    Wed, Dec 20, 2023, 4:37 PM +01:00

--- watch/watch.py
+++ watch/watch.py
@@ -4,7 +4,6 @@ import sys
 def main():
     print(parse(input("HTML: ").strip()))
 
-
 def parse(s):
     if matches := re.search(r"^<iframe.+src=\"https?://(?:www.)?youtube.com/embed/([a-zA-Z0-9]+)\".*></iframe>$", s):
         return f"https://youtu.be/{matches.group(1)}"
