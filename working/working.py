bc34fe20c9ecbf0a107b0cdf3159d18074962b1f
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-20 18:58:47 +0000

    Wed, Dec 20, 2023, 7:58 PM +01:00

--- working/working.py
+++ working/working.py
@@ -10,8 +10,9 @@ def convert(s):
         first_ampm = matches.group(2)
         second_number = matches.group(3)
         second_ampm = matches.group(4)
-        print(first_number, second_number, first_ampm, second_ampm, sep="\n")
-        return True
+
+
+
     else:
         raise ValueError
 
@@ -19,6 +20,8 @@ def convert(s):
 if __name__ == "__main__":
     main()
 
+
+# (r"^((?:[1-9]|10|11)(?::[0-5][0-9])?|12(?::00)?) (AM|PM) to ((?:[1-9]|10|11)(?::[0-5][0-9])?|12(?::00)?) (AM|PM)", s):
 # (r"^(([1-9]|10|11)(:[0-5][0-9])?|12(:00)?) (AM|PM) to (([1-9]|10|11)(:[0-5][0-9])?|12(:00)?) (AM|PM)", s):
 # (([1-9]|10|11)(:[0-5][0-9])?|12(:00)?)
 
