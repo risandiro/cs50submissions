ca3c9f838a0afef5a720acd8a00f3fdbb1acdef2
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-20 18:01:41 +0000

    Wed, Dec 20, 2023, 7:01 PM +01:00

--- working/working.py
+++ working/working.py
@@ -6,7 +6,7 @@ def main():
 
 def convert(s):
     # if matches := re.search(r"^ (AM|PM) to  (AM|PM)$", s):
-    if matches := re.search(r"^([0-9]|10|11) $", s):
+    if matches := re.search(r"^(([0-9]|10|11)(:[0-5][0-9][0-5][0-9])?|12(:00)?) $", s):
         return True
     else:
         raise ValueError
@@ -14,3 +14,6 @@ def convert(s):
 
 if __name__ == "__main__":
     main()
+
+
+# (([0-9]|10|11)(:[0-5][0-9][0-5][0-9])?|12(:00)?)
