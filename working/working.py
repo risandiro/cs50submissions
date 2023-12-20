d79d47b98a0476279ef673ca856bf7a96fb2ed25
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-20 17:50:58 +0000

    Wed, Dec 20, 2023, 6:50 PM +01:00

--- working/working.py
+++ working/working.py
@@ -7,10 +7,7 @@ def main():
 def convert(s):
     # if matches := re.search(r"^ (AM|PM) to  (AM|PM)$", s):
     if matches := re.search(r"^([0-9]{1,2}(?::[0-9]{2})?) (AM|PM) to ([0-9]{1,2}(?::[0-9]{2})?) (AM|PM)$", s):
-        first_time, second_time, x, y = matches.groups()
-        if ":" in first_time:
-            first_time_hours, first_time_seconds = first_time.split(":")
-            if first_time_hours
+
 
 
 
