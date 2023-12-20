7fa155006a67cfbee4bdb2ec3238033d94684b94
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-20 18:55:59 +0000

    Wed, Dec 20, 2023, 7:55 PM +01:00

--- working/working.py
+++ working/working.py
@@ -5,10 +5,11 @@ def main():
     print(convert(input("Hours: ").strip()))
 
 def convert(s):
-    if matches := re.search(r"^(([1-9]|10|11)(:[0-5][0-9])?|12(:00)?) (AM|PM) to (([1-9]|10|11)(:[0-5][0-9])?|12(:00)?) (AM|PM)", s):
+    if matches := re.search(r"^((?:[1-9]|10|11)(?::[0-5][0-9])?|12(:00)?) (AM|PM) to ((?:[1-9]|10|11)(:[0-5][0-9])?|12(:00)?) (AM|PM)", s):
         first_number = matches.group(1)
-        first_ampm = matches.group(5)
-        second_number = matches.group(6)
+        first_ampm = matches.group(2)
+        second_number = matches.group(3)
+        second_ampm = matches.group(4)
         print(first_number, second_number, first_ampm, sep="\n")
         return True
     else:
@@ -18,7 +19,7 @@ def convert(s):
 if __name__ == "__main__":
     main()
 
-
+# (r"^(([1-9]|10|11)(:[0-5][0-9])?|12(:00)?) (AM|PM) to (([1-9]|10|11)(:[0-5][0-9])?|12(:00)?) (AM|PM)", s):
 # (([1-9]|10|11)(:[0-5][0-9])?|12(:00)?)
 
 # ([1-9]|10|11)
