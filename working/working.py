4287205a5e805af8043487b1bd8a55011dd78666
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-20 16:02:31 +0000

    Wed, Dec 20, 2023, 5:02 PM +01:00

--- working/working.py
+++ working/working.py
@@ -5,7 +5,9 @@ def main():
     print(convert(input("Hours: ").strip()))
 
 def convert(s):
-    if matches := re.search(r" to (?:[0-9]|(?:10|11|12))", s)
+    if matches := re.search(r" to (?:[0-9]|(?:10|11|12)", s):
+        return True
+    return False
 
 
 
