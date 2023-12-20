1109169b9a94d5c6be742494574bdbf6ce86fc25
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-20 18:31:03 +0000

    Wed, Dec 20, 2023, 7:31 PM +01:00

--- working/working.py
+++ working/working.py
@@ -6,6 +6,8 @@ def main():
 
 def convert(s):
     if matches := re.search(r"^(([1-9]|10|11)(:[0-5][0-9])?|12(:00)?) (AM|PM) to (([1-9]|10|11)(:[0-5][0-9])?|12(:00)?) (AM|PM)", s):
+        a = matches.group(2)
+        print(a)
         return True
     else:
         raise ValueError
