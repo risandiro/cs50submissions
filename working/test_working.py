65faabc87facc0c534e0ac10c94592f1d5625fe5
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-21 18:42:19 +0000

    Thu, Dec 21, 2023, 7:42 PM +01:00

--- working/test_working.py
+++ working/test_working.py
@@ -20,35 +20,10 @@ def test_abnormal():
         convert("9:60 AM - 9:60 PM")
 
 
+
 def test_invalid():
     with pytest.raises(ValueError):
         convert("9 AM - 5 PM")
-        convert("09:00 AM - 17:00 PM")
-        convert("9 AM to  11 AM")
-        convert("9 AM  to 11 AM")
-        convert("9 AM to  11 AM")
-        convert("9 AM too 11 AM")
-        convert("9 AM ui 11 AM")
-        convert("9 AM TO 11 AM")
-        convert("9 AM 11 AM")
-        convert("9:20 PM bo 7 AM")
-        convert("9 AM11 AM")
-        convert("9 PM mu 11:52 AM")
-        convert("9 SM to 11:52 AM")
-        convert("9 AM to 11:52 SM")
-        convert("11:47 PM .. 1 PM")
-        convert("10:01 AM10:02 PM")
-        convert("4 AM tu 14:11 AM")
-        convert("9 to 11:52 AM")
-        convert("11:52 PM to 11:52")
-        convert("9 AM")
-        convert("9 to 5")
-        convert("9 PM to")
-        convert("9 PM to ")
-        convert("to")
-        convert(" to ")
-        convert("")
-        convert()
 
 
 
