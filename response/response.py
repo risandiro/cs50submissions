77f23a86c509d74cbdb1ab0cdef3eabb9e241d7d
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-27 22:54:35 +0000

    Wed, Dec 27, 2023, 11:54 PM +01:00

--- response/response.py
+++ response/response.py
@@ -1,12 +1,11 @@
 from validator_collection import validators
 
 def main():
-    print(validate(input("What's your email adress? ")))
+    print(validate(input("What's your email address? ")))
 
 
 def validate(email):
-    email_address = validators.email(email)
-
+    return validators.email(email)
 
 if __name__ == "__main__":
     main()
