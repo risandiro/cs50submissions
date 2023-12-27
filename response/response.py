ff7f9f2d48af34f91ebee02e217df67a26ca10e6
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-27 22:47:42 +0000

    Wed, Dec 27, 2023, 11:47 PM +01:00

--- response/response.py
+++ response/response.py
@@ -1,2 +1,12 @@
-import validators
+from validator_collection import validators
 
+def main():
+    print(validate(input("What's your email adress? ")))
+
+
+def validate(email):
+    
+
+
+if __name__ == "__main__":
+    main()
