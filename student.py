2ccccf1c338b34199b2708b897bc98c7cbd3677c
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-28 20:36:55 +0000

    Thu, Dec 28, 2023, 9:36 PM +01:00

--- student.py
+++ student.py
@@ -1,17 +1,21 @@
 def main():
+
+
+def get_student():
+    student = {}
+    student["name"] = input("Name: ")
+    student["house"] = input("House: ")
+    return student # returns a dictionary
+
+# ---------------------------------------------
+def main():
     student = get student()
     if student[0] == "Padma":
         student[1] = "Ravenclaw"
     print(f"{student[0]} from {student[1]}")
 
 def get_student():
-    student = {}
-    student["name"] = input("Name: ")
-    student["house"] = input("House: ")
-    # return student -> a dictionary
-
-def get_student():
     name = input("Name: ")
     house = input("House: ")
     # return (name, house) -> a tuple
-    # return [name, house] -> a list
+    return [name, house] # returns a list
