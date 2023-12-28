73c5a754d0d420373073fcf1d99720fa56b6fa4c
risandiro <151243329+risandiro@users.noreply.github.com>
2023-12-28 20:32:14 +0000

    Thu, Dec 28, 2023, 9:32 PM +01:00

---
+++ student.py
@@ -0,0 +1,12 @@
+def main():
+    student = get student()
+    if student[0] == "Padma":
+        student[1] = "Ravenclaw"
+    print(f"{student[0]} from {student[1]}")
+
+def get_student():
+    student = {}
+    student["name"] = input("Name: ")
+    student["house"] = input("House: ")
+    return student
+
