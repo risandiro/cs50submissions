3419ed047334649a20082257ee825bb39a771eee
risandiro <151243329+risandiro@users.noreply.github.com>
2024-02-01 20:01:51 +0000

    Thu, Feb 1, 2024, 9:01 PM +01:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -2,44 +2,28 @@ import sys, re
 from datetime import date
 
 def main():
-    get_date()
-    validate_date()
-
+    x = get_date()
+    validate_date(x)
 
 def get_date():
+     try:
+        prompt = input("Date of Birth: ").strip()
+        if not re.search(r"^[0-9]{4}-[0-9]{2}-[0-9]{2}$", prompt): sys.exit("Invalid date")
+        return (prompt)
+
+def validate_date(s):
     today = str(date.today())
     y, m, d = today.split("-")
     y, m, d = int(y), int(m), int(d)
 
     try:
-        prompt = input("Date of Birth: ").strip()
-        if not re.search(r"^[0-9]{4}-[0-9]{2}-[0-9]{2}$", prompt): raise ValueError
-        year, month, day = prompt.split("-")
+        year, month, day = s.split("-")
         year, month, day = int(year), int(month), int(day)
         if year < 1 or year > y or month < 1 or month > 12 or day < 1 or day > 31: raise ValueError
         if year == y:
             if month > m: raise ValueError
             if month == m:
                 if day > d: raise ValueError
-
-    except ValueError:
-        sys.exit("Invalid date")
-
-def get_date():
-     try:
-        prompt = input("Date of Birth: ").strip()
-        if not re.search(r"^[0-9]{4}-[0-9]{2}-[0-9]{2}$", prompt): sys.exit("Invalid date")
-        year, month, day = prompt.split("-")
-        return (year, month, day)
-
-
-def validate_date()
-    today = str(date.today())
-    y, m, d = today.split("-")
-    y, m, d = int(y), int(m), int(d)
-
-    try:
-
     except ValueError:
         sys.exit("Invalid date")
 
