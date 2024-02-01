e082e19972f0f1d86bc5457f33dc9cef09501ced
risandiro <151243329+risandiro@users.noreply.github.com>
2024-02-01 18:43:08 +0000

    Thu, Feb 1, 2024, 7:43 PM +01:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,4 +1,15 @@
-import inflect
-
 from datetime import date
 
+def main():
+
+
+def get_input():
+    try:
+        date = input("Date of Birth:" )
+        day, month, year = date.split("-")
+        print(day)
+
+
+
+if __name__ == "__main__":
+    main()
