1631b276d0209526ad8df8cb774eebfbc6821fa4
risandiro <151243329+risandiro@users.noreply.github.com>
2024-02-01 19:19:13 +0000

    Thu, Feb 1, 2024, 8:19 PM +01:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -13,8 +13,8 @@ def get_date():
     try:
         prompt = input("Date of Birth: ")
         year, month, day = prompt.split("-")
-        if len(year) == 4 and len(month) == 2 and len(day) == 2:
-            year, month, day = int(year), int(month), int(day)
+        if len(year) != 4 and len(month) != 2 and len(day) != 2: raise ValueError
+        year, month, day = int(year), int(month), int(day)
 
 
     except ValueError:
