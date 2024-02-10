1f4019aec40f052d1d1aede973ee887f1b7addcd
risandiro <151243329+risandiro@users.noreply.github.com>
2024-02-10 21:09:58 +0000

    Sat, Feb 10, 2024, 10:09 PM +01:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -2,9 +2,14 @@ from seasons import validate, convert, time_to_words
 import pytest
 
 def test_validate():
-    assert validate(")
+    with pytest.raises(ValueError):
+        validate("4000-11-11")
+        validate("1111-40-11")
+        validate("1111-11-40")
+        validate("xx-xx-xx")
 
 def test_convert():
+    with pytest.raises(ValueError):
 
 def test_time_to_words():
 
