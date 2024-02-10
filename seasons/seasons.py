067c63fae96b3a45a12e2983036f5bd2f169a0c3
risandiro <151243329+risandiro@users.noreply.github.com>
2024-02-10 19:35:54 +0000

    Sat, Feb 10, 2024, 8:35 PM +01:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,4 +1,23 @@
+import sys
 from datetime import date
 
-x = date.today()
-print(x)
+def main():
+    current = date.today()
+    user_input = input("Date of Brith: ")
+    user_input = validate(user_input)
+    print(user_input)
+
+
+def validate(inp):
+    try:
+        year, month, day = inp.split("-")
+        year, month, day =
+        user_input = date(year, month, day)
+        return user_input
+
+    except ValueError:
+        sys.exit("Invalid date")
+
+
+if __name__ == "__main__":
+    main()
