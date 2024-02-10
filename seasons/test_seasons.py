8d3ff0249fbe679ee86d35b712a99cd48cdffbd7
risandiro <151243329+risandiro@users.noreply.github.com>
2024-02-10 21:18:43 +0000

    Sat, Feb 10, 2024, 10:18 PM +01:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -2,11 +2,10 @@ from seasons import validate, time_to_words
 import pytest
 
 def test_validate():
-    with pytest.raises(ValueError):
-        validate("4000-11-11")
-        validate("1111-40-11")
-        validate("1111-11-40")
-        validate("xx-xx-xx")
+    assert validate("4000-11-11") == "SystemExit: Invalid date"
+    assert validate("1111-40-11") == "SystemExit: Invalid date"
+    assert validate("1111-11-40") == "SystemExit: Invalid date"
+    assert validate("xx-xx-xx") == "SystemExit: Invalid date"
 
 def test_time_to_words():
     assert time_to_words(1440) == ("One thousand, four hundred forty minutes")
