69ba540f8b1902f2b55bba67c93ba93664ff3d8e
risandiro <151243329+risandiro@users.noreply.github.com>
2024-02-11 21:57:39 +0000

    Sun, Feb 11, 2024, 10:57 PM +01:00

--- jar/test_jar.py
+++ jar/test_jar.py
@@ -1,14 +1,15 @@
 from jar import Jar
 
 def test_init():
-    ...
+    jar = Jar(7)
+    assert capacity(jar) == 7
 
 def test_str():
     jar = Jar()
     assert str(jar) == ""
     jar.deposit(1)
     assert str(jar) ==  "🍪"
-    jar deposit(11)
+    jar.deposit(11)
     assert str(jar) ==  "🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪"
 
 def test_deposit():
