622630a9e855257bba24ee8b985efe2f9c2ec27a
risandiro <151243329+risandiro@users.noreply.github.com>
2024-02-11 19:49:33 +0000

    Sun, Feb 11, 2024, 8:49 PM +01:00

--- jar/jar.py
+++ jar/jar.py
@@ -1,10 +1,14 @@
 class Jar:
     def __init__(self, capacity=12):
-        if self.capacity
+        if capacity < 0:
+            raise ValueError("Wrong capacity")
+        self.capacity = capacity
+        self.size = 0
 
     def ___str___(self):
-        ...
+        return self.size * "🍪"
 
+'''
     def deposit(self, n):
         ...
 
@@ -16,5 +20,7 @@ class Jar:
 
     @property
     def size(self):
+'''
 
-
+jar = Jar()
+print(jar)
