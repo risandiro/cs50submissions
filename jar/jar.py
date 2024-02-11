f213a87e0db38eb5b62eb87e62068a1f0c849f21
risandiro <151243329+risandiro@users.noreply.github.com>
2024-02-11 20:05:54 +0000

    Sun, Feb 11, 2024, 9:05 PM +01:00

--- jar/jar.py
+++ jar/jar.py
@@ -2,29 +2,29 @@ class Jar:
     def __init__(self, capacity=12):
         if capacity < 0:
             raise ValueError("Capacity is too small")
-        self.capacity = capacity
-        self.size = 0
+        self._capacity = capacity
+        self._size = 0
 
     def __str__(self):
-        return self.size * "🍪"
+        return self._size * "🍪"
 
     def deposit(self, n):
         if self.size + n > self.capacity:
             raise ValueError("Jar is not big enough")
-        self.size = self.size + n
+        self._size = self.size + n
 
     def withdraw(self, n):
         if self.size < n:
             raise ValueError("Not enough cookies in the jar")
-        self.size = self.size - n
+        self._size = self.size - n
 
     @property
     def capacity(self):
-        return self.capacity
+        return self._capacity
 
     @property
     def size(self):
-        return self.size
+        return self._size
 
 jar = Jar()
 jar.capacity()
