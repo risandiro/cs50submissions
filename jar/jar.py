149c2d06d22e82295b96827526ae385d39b3d53e
risandiro <151243329+risandiro@users.noreply.github.com>
2024-02-11 19:37:14 +0000

    Sun, Feb 11, 2024, 8:37 PM +01:00

--- jar/jar.py
+++ jar/jar.py
@@ -1,2 +1,20 @@
 class Jar:
-    
+    def __init__(self, capacity=12):
+        ...
+
+    def ___str___(self):
+        ...
+
+    def deposit(self, n):
+        ...
+
+    def withdraw(self, n):
+        ...
+
+    @property
+    def capacity(self):
+
+    @property
+    def size(self):
+
+
