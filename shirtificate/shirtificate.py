64446a6551d355355ba9d52d39ed0182c3d1273a
risandiro <151243329+risandiro@users.noreply.github.com>
2024-02-12 15:26:12 +0000

    Mon, Feb 12, 2024, 4:26 PM +01:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -4,8 +4,8 @@ class PDF():
     def __init__(self, name):
         self._pdf = FPDF(orientation="portrait", format="A4")
         self._pdf.add_page()
-        self._pdf.set_font("helvetica", "B", 40)
-        self._pdf.cell(0, 60, "CS50 Shirtificate", border=0, align="C")
+        self._pdf.set_font("helvetica", "", 40)
+        self._pdf.cell(0, 60, "CS50 Shirtificate", align="C")
 
     def save(self, title):
         self._pdf.output(title)
