ecb3f670aa3b051181b2ee2e400e336e3261c445
risandiro <151243329+risandiro@users.noreply.github.com>
2024-02-12 15:13:32 +0000

    Mon, Feb 12, 2024, 4:13 PM +01:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -2,7 +2,9 @@ from fpdf import FPDF
 
 class PDF():
     def __init__(self, name):
-        self._pdf = FPDF()
+        self._pdf = FPDF(orientation="portrait", format="A4")
+        self._pdf.add_page()
+        self._pdf.set_font("helvetica", "B", 15)
 
 
 
