3e23ce08ce94ca1d6e2930092dba013bec483385
risandiro <151243329+risandiro@users.noreply.github.com>
2024-02-16 22:46:13 +0000

    Fri, Feb 16, 2024, 11:46 PM +01:00

--- bank.py
+++ bank.py
@@ -1,6 +1,6 @@
-def meow(n: int) -> None:
-    for _ in range(n):
-        print("meow")
+def meow(n: int) -> str:
+    return "meow\n" * n
 
-number: int = input("Number: ")
-meow(number)
+number: int = int(input("Number: "))
+meows: str = meow(number)
+print(meows)
