7b02d17e9576a4b805ffaf3495fda90007967521
risandiro <151243329+risandiro@users.noreply.github.com>
2024-02-18 18:59:00 +0000

    Sun, Feb 18, 2024, 7:59 PM +01:00

--- bank.py
+++ bank.py
@@ -7,9 +7,13 @@ print(meows, end="")
 
 
 
-"""
-Meow n times.
+    """
+    Meow n times.
 
-:param n: Number of times to meow
-
+    :param n: Number of times to meow
+    :type n: int
+    :raise TypeError: If n is not an int
+    :return: A string of n meows, one per line
+    :rtype: str
+    """
 
