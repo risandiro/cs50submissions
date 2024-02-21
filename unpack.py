fbdd7cd89e46d4dd5955cc5e53809282dfe0e5a2
risandiro <151243329+risandiro@users.noreply.github.com>
2024-02-21 22:51:21 +0000

    Wed, Feb 21, 2024, 11:51 PM +01:00

--- unpack.py
+++ unpack.py
@@ -4,3 +4,15 @@ def total(galleons, sickles, knuts, pennies=0):
 coins = {"galleons": 100, "sickles": 50, "knuts": 25}
 
 print(total(**coins), "Knuts")
+
+
+def f(*args):
+    print("Numbers:", args)
+
+f(100,50,25)
+
+def f(**kwargs):
+    print("Named:", kwargs)
+
+f(galleons=100, sickles=50, knuts=25)
+
