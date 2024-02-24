0820a827930c7b009cd917cb41e8cdd788fbd7b1
risandiro <151243329+risandiro@users.noreply.github.com>
2024-02-24 19:29:30 +0000

    Sat, Feb 24, 2024, 8:29 PM +01:00

--- gryffindor.py
+++ gryffindor.py
@@ -1,27 +1,8 @@
-students = [
-    {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
-    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
-    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russel terrier"},
-    {"name": "Draco", "house": "Slytherin", "patronus": None}
-]
+stocks = {
+    "AAPL": 187.31,
+    "MSFT": 124.06,
+    "FB": 183.50
+    }
 
-def is_gryffindor(s):
-    return s["house"] == "Gryffindor"
-
-gryffindors = filter(is_gryffindor, students)
-
-sort = []
-for gryffindor in sorted(gryffindors, key=lambda s: s["name"]):
-    sort.append(gryffindor["name"])
-print (*sort, sep="\n")
-
-gryffindors = [{"name": student, "house": "Gryffindor"} for student in students]
-
-# I want a list, inside of that list I want a dictionary with key "name",
-# containing a value of each iteration of the students list, and another
-# key "house", containing a fixed string "Gryffindor"
-
-students = ["Hermione", "Harry", "Ron"]
-
-for i, student in enumerate(students):
-    print(i + 1, student)
+for key,value in stocks.items():
+    print(key + " : " + str(value))
