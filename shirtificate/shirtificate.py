f3dfd7fadb743cff10d778a619ae10490ef655b7
risandiro <151243329+risandiro@users.noreply.github.com>
2024-02-27 14:46:53 +0000

    Tue, Feb 27, 2024, 3:46 PM +01:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -2,18 +2,18 @@ from fpdf import FPDF
 
 class Shirt():
     def __init__(self, name):
-        self.shirt = FPDF(orientation="portrait", format="A4")
-        self.shirt.add_page()
-        self.shirt.set_font("helvetica", "", 40)
-        self.shirt.cell(0, 40, "CS50 Shirtificate", align="C", new_x="LMARGIN", new_y="NEXT")
-        self.shirt.image("../shirtificate/shirtificate.png", w=self.shirt.epw, y=60)
+        self._shirt = FPDF(orientation="portrait", format="A4")
+        self._shirt.add_page()
+        self._shirt.set_font("helvetica", "", 40)
+        self._shirt.cell(0, 40, "CS50 Shirtificate", align="C", new_x="LMARGIN", new_y="NEXT")
+        self._shirt.image("../shirtificate/shirtificate.png", w=self._shirt.epw, y=60)
 
-        self.shirt.set_font_size(25)
-        self.shirt.set_text_color(255, 255, 255)
-        self.shirt.cell(self.shirt.epw, 160, f"{name} took CS50", border=0, align='C')
+        self._shirt.set_font_size(25)
+        self._shirt.set_text_color(255, 255, 255)
+        self._shirt.cell(self.shirt.epw, 160, f"{name} took CS50", border=0, align='C')
 
     def save(self, title):
-        self.shirt.output(title)
+        self._shirt.output(title)
 
 def main():
     name = input("Name: ")
