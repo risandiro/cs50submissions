b0df502c08745ae94295ac95cba5303b4b5c59e6
risandiro <151243329+risandiro@users.noreply.github.com>
2024-02-28 15:54:13 +0000

    Wed, Feb 28, 2024, 4:54 PM +01:00

--- cat.py
+++ cat.py
@@ -1,22 +1,3 @@
-'''
-i = 0
-while i < 3:
-    print("meow")
-    i += 1
-
-for _ in range(3):
-    print("meow")
-----------------------------------
-while True:
-    n = int(input("What's n? "))
-    if n > 0:
-        break
-
-for _ in range(n):
-    print("meow")
-
-'''
-
 def main():
     number = get_number()
     meow(number)
