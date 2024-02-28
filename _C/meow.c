35d616f98a2a0dd2e8ebbc841cdabc6b82fda164
risandiro <151243329+risandiro@users.noreply.github.com>
2024-02-28 18:11:29 +0000

    Wed, Feb 28, 2024, 7:11 PM +01:00

--- _C/meow.c
+++ _C/meow.c
@@ -2,10 +2,16 @@
 
 int main(void)
 {
-    int i = 3;
-    while (i > 0)
+    int i = 0;
+    while (i < 3)
     {
         printf("meow\n");
-        i--;
+        i++;
     }
+
+    printf("-----\n");
+
+    // if you have only one line of code in a loop, you can skip {}
+    for(int in = 0; in < 3; in++)
+        printf("meow\n");
 }
