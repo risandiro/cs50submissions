539b5ab167c704530af6293a18ce7a3480010f8c
risandiro <151243329+risandiro@users.noreply.github.com>
2024-02-28 17:48:57 +0000

    Wed, Feb 28, 2024, 6:48 PM +01:00

---
+++ _C/agree.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    char c = get_char("Do you agree? ") // double quotes should be used for multiple characters
+
+    if (c == 'y')
+    {
+        print("Agreed.\n")
+    }
+    else if(c =='n') // single quotes should be used for a single character
+    {
+        printf("Not agreed.\n)
+    }
+}
