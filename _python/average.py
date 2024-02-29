4dbb5b60661704cf5c2513ce615ab4de2c9f65e1
risandiro <151243329+risandiro@users.noreply.github.com>
2024-02-29 19:06:51 +0000

    Thu, Feb 29, 2024, 8:06 PM +01:00

--- _python/average.py
+++ _python/average.py
@@ -1,8 +1,4 @@
 # docs.python.org/3/library/random.html
-# import statistics
+import statistics
 
-# print(statistics.mean([100, 90]))
-
-auto = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-print(auto[2:-2:2])
+print(statistics.mean([100, 90]))
