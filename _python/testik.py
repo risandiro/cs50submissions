48f784f249c0a23124a1d95735d16782b8aece0d
risandiro <151243329+risandiro@users.noreply.github.com>
2024-03-01 19:56:07 +0000

    Fri, Mar 1, 2024, 8:56 PM +01:00

---
+++ _python/testik.py
@@ -0,0 +1,12 @@
+stocks = {
+    "AAPL": 187.31,
+    "MSFT": 124.06,
+    "FB": 183.50
+    }
+
+keys = []
+values = []
+for key, value in stocks.items():
+    keys.append(key)
+    values.append(value)
+print()
