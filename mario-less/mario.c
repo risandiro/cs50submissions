63b6c179b6fa85e73e86c1dfea84974e09e1bf73
risandiro <151243329+risandiro@users.noreply.github.com>
2024-03-06 16:54:30 +0000

    Wed, Mar 6, 2024, 5:54 PM +01:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -6,20 +6,30 @@ int main(void)
     int height;
     do
     {
-        height = get_int("Height: ");
+        n = get_int("Height: ");
     }
-    while(height < 1);
+    while(n < 1);
 
 
-    for (int j = 0; j < height; j++) {
-        for (int i = 0; i < height; i++) {
-            if (i + 1 < height) {
-                printf(" ");
-
-            } else {
-                printf("#");
-            }
-        }
-        printf("\n");
+    for (int j = 0; j < n; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int k = 0; )
     }
 }
+
+
+
+
+// n = 4
+// j=4, j=3, j=2, j=1
+    // i=4, i=3, i=2, i=1
+
+//   #
+//  ##
+// ###
+//####
+
+// print j-1 (3) space + n - (j-1) hash (4 - 3 = 1) + \n
+// print j-1 (2) space + n - (j-1) hash (4 - 2 = 2) + \n
+// print j-1 (1) space + n - (j-1) hash (4 - 1 = 3) + \n
+// print j-1 (0) space + n - (j-1) hash (4 - 0 = 4) + \n
