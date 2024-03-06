3746d5125e627f70329e90a5f50d1f8ced869c9c
risandiro <151243329+risandiro@users.noreply.github.com>
2024-03-06 20:16:21 +0000

    Wed, Mar 6, 2024, 9:16 PM +01:00

--- cash/cash.c
+++ cash/cash.c
@@ -1,6 +1,8 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int calculate_quarters(int cents);
+
 int main(void)
 {
     int cents;
@@ -9,4 +11,11 @@ int main(void)
         cents = get_int("Change owed: ");
     }
     while (cents < 0);
+}
+
+int quarters = calculate_quarters(cents);
+
+int calculate_quarters(int cents)
+{
+
 }
