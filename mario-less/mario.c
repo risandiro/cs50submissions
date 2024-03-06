730434ccc10cdbf95efaa7658879b054b2d49d16
risandiro <151243329+risandiro@users.noreply.github.com>
2024-03-06 18:32:04 +0000

    Wed, Mar 6, 2024, 7:32 PM +01:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -14,9 +14,8 @@ int main(void)
 
 
     for (int i = 0; i < n; i++) {
-        int counter = 0;
         for (int j = 0; j < n; j++) {
-            if (j- (i-1) < 0) {
+            if () {
                 printf("#");
             }
             else {
