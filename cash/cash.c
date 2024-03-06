32b24d68eacd6d81caf71de45e9e1e2edac0a4f0
risandiro <151243329+risandiro@users.noreply.github.com>
2024-03-06 20:56:35 +0000

    Wed, Mar 6, 2024, 9:56 PM +01:00

--- cash/cash.c
+++ cash/cash.c
@@ -18,11 +18,27 @@ int main(void)
 
 int calculate_coins(int cents)
 {
-    int counter_quarters = 0;
+    int quarters = 0;
     while (cents - 25 >= 0)
     {
-        counter_quarters++;
+        quarters++;
         cents = cents - 25;
     }
-    return counter_quarters;
+
+    int nickels = 0;
+    while (cents - 10 >= 0)
+    {
+        nickels++;
+        cents = cents - 10;
+    }
+
+    int pennies = 0;
+    while (cents - 5 >= 0)
+    {
+        pennies++;
+        cents = cents - 5;
+    }
+
+    int total = quarters + nickels + pennies + cents;
+    return total;
 }
