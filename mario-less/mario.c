fe53034e4fb8e031e8070346d9b80ed002a8765c
risandiro <151243329+risandiro@users.noreply.github.com>
2024-03-06 19:13:25 +0000

    Wed, Mar 6, 2024, 8:13 PM +01:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,32 +1,33 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
 void print_row(int spaces, int bricks);
 
 int main(void)
 {
     int n;
-    do {
+    do
+    {
         n = get_int("Height: ");
     }
     while (n < 1);
 
-
-    for (int i = 0; i < n; i++) {
-
+    for (int i = 0; i < n; i++)
+    {
         print_row(n - (i + 1), i + 1);
     }
 }
 
 void print_row(int spaces, int bricks) {
 
-    for (int x = 0; x < spaces; x++) {
+    for (int x = 0; x < spaces; x++)
+    {
         printf(" ");
     }
 
-    for (int x = 0; x < bricks; x++) {
+    for (int x = 0; x < bricks; x++)
+    {
         printf("#");
     }
     printf("\n");
 }
-
