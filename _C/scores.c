d8946c065de8588e3828005c74e789e8f6f26af6
risandiro <151243329+risandiro@users.noreply.github.com>
2024-03-07 19:59:09 +0000

    Thu, Mar 7, 2024, 8:59 PM +01:00

--- _C/scores.c
+++ _C/scores.c
@@ -6,10 +6,6 @@ int main(void)
     int score2 = 73;
     int score3 = 33;
 
-    printf("Average: %f\n", (score1 + score2 + score3) / 3.0 )
+    printf("Average: %f\n", (score1 + score2 + score3) / 3.0 );
 }
 
-/*
-Easiest way to switch from integer math to floating point math is to have
-at least 1 floating number by changing the denominator by adding .0
-*/
