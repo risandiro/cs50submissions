6d73e595f777ac0ca069b6034ea795535ff18bf8
risandiro <151243329+risandiro@users.noreply.github.com>
2024-03-07 20:15:23 +0000

    Thu, Mar 7, 2024, 9:15 PM +01:00

--- _C/scores.c
+++ _C/scores.c
@@ -7,9 +7,12 @@ int main(void)
     int score3 = 33;
 
     printf("Average: %f\n", (score1 + score2 + score3) / 3.0 );
-}
 
-int scores[3];
-scores[0] = 72;
-scores[1] = 73;
-scores[2] = 33;
+
+    int scores[3];
+    scores[0] = 72;
+    scores[1] = 73;
+    scores[2] = 33;
+
+    printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
+}
