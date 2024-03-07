0f28dcc114e27ce75885616cf62f762c0517b81e
risandiro <151243329+risandiro@users.noreply.github.com>
2024-03-07 20:16:58 +0000

    Thu, Mar 7, 2024, 9:16 PM +01:00

--- _C/scores.c
+++ _C/scores.c
@@ -1,3 +1,4 @@
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
@@ -10,9 +11,9 @@ int main(void)
 
 
     int scores[3];
-    scores[0] = 72;
-    scores[1] = 73;
-    scores[2] = 33;
+    scores[0] = get_int("Score: ");
+    scores[1] = get_int("Score: ");
+    scores[2] = get_int("Score: ");
 
     printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
 }
