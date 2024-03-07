7fbe05e1807f14913053c3ef8780b9f9f419a1ad
risandiro <151243329+risandiro@users.noreply.github.com>
2024-03-07 20:19:24 +0000

    Thu, Mar 7, 2024, 9:19 PM +01:00

--- _C/scores.c
+++ _C/scores.c
@@ -10,10 +10,18 @@ int main(void)
     printf("Average: %f\n", (score1 + score2 + score3) / 3.0 );
 
 
+    /*
     int scores[3];
     scores[0] = get_int("Score: ");
     scores[1] = get_int("Score: ");
     scores[2] = get_int("Score: ");
+    */
+
+    int scores[3];
+    for (int i = 0; i < 3; i++)
+    {
+        scores[i] = get_int("Score: ");
+    }
 
     printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
 }
