65a3cf12cd6b8dbd9a42c58856d96fbb9db76966
risandiro <151243329+risandiro@users.noreply.github.com>
2024-03-07 20:50:09 +0000

    Thu, Mar 7, 2024, 9:50 PM +01:00

--- _C/scores.c
+++ _C/scores.c
@@ -3,18 +3,12 @@
 
 int main(void)
 {
+    /*
     int score1 = 72;
     int score2 = 73;
     int score3 = 33;
 
     printf("Average: %f\n", (score1 + score2 + score3) / 3.0 );
-
-
-    /*
-    int scores[3];
-    scores[0] = get_int("Score: ");
-    scores[1] = get_int("Score: ");
-    scores[2] = get_int("Score: ");
     */
 
     int reps = get_int("How many scores? ");
---
+++ _C/scorez.c
@@ -0,0 +1,8 @@
+#include <cs50.h>
+#include <stdio.h>
+
+// Constant
+const int N = 3;
+
+// Prototype
+float average(int lenght, int array[]);
