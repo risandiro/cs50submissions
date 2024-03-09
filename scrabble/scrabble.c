7d855fec8e507f7cdb6131fc4f6ed11a4c2e0855
risandiro <151243329+risandiro@users.noreply.github.com>
2024-03-09 19:43:35 +0000

    Sat, Mar 9, 2024, 8:43 PM +01:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -3,9 +3,25 @@
 #include <ctype.h>
 #include <string.h>
 
+
+int compute_score(string word);
+
 int main(void)
 {
-    string player1 = get_string("Player 1: ");
-    string player2 = get_string("Player 2: ");
-    
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
+
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
+
+    if (score1 > score2)
+    {
+        printf("Player");
+    }
+
+}
+
+int compute_score(string word)
+{
+
 }
