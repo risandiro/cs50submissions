e98aeb25d07d3b4bdbf9b5d9338e5ae01fb7a9ab
risandiro <151243329+risandiro@users.noreply.github.com>
2024-03-09 20:33:13 +0000

    Sat, Mar 9, 2024, 9:33 PM +01:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -1,15 +1,13 @@
 #include <cs50.h>
-#include <stdio.h>
 #include <ctype.h>
+#include <stdio.h>
 #include <string.h>
 
 
 int compute_score(string word);
 
-int POINTS[] = {
-    // A == 1; B == 3; C == 3; D == 2; E == 1...
-    1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10
-};
+// A == 1; B == 3; C == 3; D == 2; E == 1...
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 int main(void)
 {
@@ -26,15 +24,12 @@ int main(void)
     else if (score1 < score2)
     {
          printf("Player 2 wins!");
-
     }
     else
     {
         printf("Tie!");
-
     }
     printf("\n");
-
 }
 
 int compute_score(string word)
