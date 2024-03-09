2bde3f385f67652b365916d675dcc9def18a5937
risandiro <151243329+risandiro@users.noreply.github.com>
2024-03-09 19:48:20 +0000

    Sat, Mar 9, 2024, 8:48 PM +01:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -6,6 +6,10 @@
 
 int compute_score(string word);
 
+int POINTS[] = {
+    1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10
+}
+
 int main(void)
 {
     string word1 = get_string("Player 1: ");
@@ -16,8 +20,19 @@ int main(void)
 
     if (score1 > score2)
     {
-        printf("Player");
+        printf("Player 1 wins!");
     }
+    else if (score1 < score2)
+    {
+         printf("Player 2 wins!");
+
+    }
+    else
+    {
+        printf("Tie!");
+
+    }
+    printf("\n");
 
 }
 
