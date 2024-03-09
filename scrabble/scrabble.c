c1b59d4ffccd225d37f10eb32ab127c834ade659
risandiro <151243329+risandiro@users.noreply.github.com>
2024-03-09 19:40:41 +0000

    Sat, Mar 9, 2024, 8:40 PM +01:00

---
+++ scrabble/scrabble.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int main(void)
+{
+    string player1 = get_string("Player 1: ");
+    string player2 = get_string("Player 2: ");
+    
+}
