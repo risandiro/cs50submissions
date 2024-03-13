4237ff5973aee6055de228b9775995aa6d5a21fb
risandiro <151243329+risandiro@users.noreply.github.com>
2024-03-13 20:11:32 +0000

    Wed, Mar 13, 2024, 9:11 PM +01:00

--- readability/readability.c
+++ readability/readability.c
@@ -51,7 +51,7 @@ int count_letters(string text)
 
 int count_words(string text)
 {
-    int counter = 0;
+    int counter = 1;
     for (int i = 0, len = strlen(text); i < len; i++)
     {
         if (text[i] == ' ' && text[i+1] != ' ')
