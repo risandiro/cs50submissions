38726b7a628850ec3b3b05c84b383c3f6f547b52
risandiro <151243329+risandiro@users.noreply.github.com>
2024-03-14 20:50:14 +0000

    Thu, Mar 14, 2024, 9:50 PM +01:00

--- ceasar/ceasar.c
+++ ceasar/ceasar.c
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
     {
         if (isalpha(plaintext[j]))
         {
-            printf("%c", plaintext[j] - 97);
+            printf("%i", plaintext[j] - 97);
         }
         else
         {
