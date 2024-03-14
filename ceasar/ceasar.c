56f6e82fefd3c2d4ff18ba38451b2fa7f1bf6f0f
risandiro <151243329+risandiro@users.noreply.github.com>
2024-03-14 20:36:33 +0000

    Thu, Mar 14, 2024, 9:36 PM +01:00

--- ceasar/ceasar.c
+++ ceasar/ceasar.c
@@ -39,7 +39,7 @@ int main(int argc, char *argv[])
         }
         else
         {
-            printf("%c", plaintext[j] + (key % 26));
+            printf("%c", ((plaintext[j] - 65) % 26) + (key % 26)) + 65;
         }
     }
     printf("\n");
