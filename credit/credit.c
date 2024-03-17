be55ac1b930b670307e825263176a623884e29c9
risandiro <151243329+risandiro@users.noreply.github.com>
2024-03-17 00:35:29 +0000

    Sun, Mar 17, 2024, 1:35 AM +01:00

--- credit/credit.c
+++ credit/credit.c
@@ -13,26 +13,16 @@ int main(void)
 
     char array[len];
     sprintf(array, "%ld", number);
-    printf("%s\n", array);
 
-
-
-    /*int sum_mul = 0, sum_nor = 0;
-    sprintf(strnum, "%c", number);
-
-    for (int i = len - 2; i >= 0; i-=2)
+    for (int i = len - 2, sum_mul = 0; i >= 0; i-=2)
     {
-        sum_mul += strnum[i];
+        sum_mul += array[i] * 2;
     }
 
-    for (int i = len - 1; i >= 0; i-=2)
+    for (int i = len - 1, sum_nor = 0; i >= 0; i-=2)
     {
-        sum_nor += strnum[i];
+        sum_nor += array[i] - 0;
     }
-
-    printf("multiplied: %i", sum_mul);
-    printf("\nnormal: %i\n", sum_nor);*/
-
 }
 
 int longlen(long num)
