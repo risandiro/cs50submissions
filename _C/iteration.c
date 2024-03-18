15816981a71634931e0977c8d27d1f3d43d2ab1b
risandiro <151243329+risandiro@users.noreply.github.com>
2024-03-18 22:51:34 +0000

    Mon, Mar 18, 2024, 11:51 PM +01:00

--- _C/iteration.c
+++
@@ -1,9 +0,0 @@
-#include <cs50.h>
-#include <stdio.h>
-
-int main(void)
-{
-    int height = get_int("Height: ");
-    
-}
-
---
+++ _C/recursion.c
@@ -0,0 +1,22 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void draw(int n);
+
+int main(void)
+{
+    int height = get_int("Height: ");
+    draw(height);
+}
+
+void draw(int n)
+{
+    // Print one more row
+    for (int i = 0; i < n; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+
+    draw(n - 1);
+}
