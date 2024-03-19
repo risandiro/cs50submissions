544197ed6577516578ecf95facdcdaeb0f9aa16c
risandiro <151243329+risandiro@users.noreply.github.com>
2024-03-19 17:08:09 +0000

    Tue, Mar 19, 2024, 6:08 PM +01:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -77,9 +77,11 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
+    int old max;
+    int new max;
     for (int i = 0; i < candidate_count; i++)
     {
-
+        
     }
 
     return;
