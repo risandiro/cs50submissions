921e3aba6091fdefa7ff284504c5c56af7615453
risandiro <151243329+risandiro@users.noreply.github.com>
2024-03-22 20:03:42 +0000

    Fri, Mar 22, 2024, 9:03 PM +01:00

--- _C/copy.c
+++ _C/copy.c
@@ -5,7 +5,9 @@
 char *original = "Hello!";
 
 char *new = malloc(strlen(s) + 1);
-for (int i = 0, len = strlen(s); i < len; i++)
+for (int i = 0, len = strlen(s); i < len + 1; i++)
 {
     new[i] = original[i];
 }
+
+// len + 1 because you want to copy the \0 value also
