7cde3cd4fa2517bc0f87f2fd9f0cfba88214e25c
risandiro <151243329+risandiro@users.noreply.github.com>
2024-03-23 19:26:39 +0000

    Sat, Mar 23, 2024, 8:26 PM +01:00

---
+++ _C/copy.c
@@ -0,0 +1,29 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+{
+    char *original = "Hello!";
+
+    char *new = malloc(strlen(original) + 1);
+    if (new == NULL)
+    {
+            return 1;
+    }
+
+    for (int i = 0, len = strlen(original); i < len + 1; i++)
+    {
+        new[i] = original[i];
+    }
+
+    if (strlen(new) > 0)
+    {
+        new[0] = isupper(new[0]);
+    }
+
+    printf("%s\n", original);
+    printf("%s\n", new);
+}
