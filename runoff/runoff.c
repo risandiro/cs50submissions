67516dce11d31abf59f9a378309124650588836c
risandiro <151243329+risandiro@users.noreply.github.com>
2024-03-24 16:47:44 +0000

    Sun, Mar 24, 2024, 5:47 PM +01:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -130,7 +130,7 @@ bool vote(int voter, int rank, string name)
     {
         if (strcmp(name, candidates[i].name) == 0)
         {
-            preferences[i][rank]++;
+            preferences[voter][rank] = i;
             return true;
         }
     }
