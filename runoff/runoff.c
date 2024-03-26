6219a36a11c89c6601503c1a3bd26d79ad5008c4
risandiro <151243329+risandiro@users.noreply.github.com>
2024-03-26 17:17:19 +0000

    Tue, Mar 26, 2024, 6:17 PM +01:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -141,7 +141,9 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    // TODO
+    // prebehni cely prvy riadok
+    // pokial kandidat nebol eliminovany
+    // zvys jeho vlas o 1
     return;
 }
 
