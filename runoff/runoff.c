37df8ada8d643a11e9ca687e1ba923000a328518
risandiro <151243329+risandiro@users.noreply.github.com>
2024-03-26 17:37:41 +0000

    Tue, Mar 26, 2024, 6:37 PM +01:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -172,8 +172,18 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    
-    return 0;
+    int min = 0;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].eliminated == false)
+        {
+            if (candidates[i].votes > min)
+            {
+                min = candidates[i].votes;
+            }
+        }
+    }
+    return min;
 }
 
 // Return true if the election is tied between all candidates, false otherwise
