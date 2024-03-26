a5e7b999aad28edb843aaa55fdf8f5f20d710c2a
risandiro <151243329+risandiro@users.noreply.github.com>
2024-03-26 17:33:18 +0000

    Tue, Mar 26, 2024, 6:33 PM +01:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -162,6 +162,7 @@ bool print_winner(void)
     {
         if (candidates[i].votes > voter_count / 2)
         {
+            printf("%s\n", candidates[i].name);
             return true;
         }
     }
