875ede0fc5df1ee004caaa5bed41501b14944cc2
risandiro <151243329+risandiro@users.noreply.github.com>
2024-04-16 19:52:45 +0000

    Tue, Apr 16, 2024, 9:52 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -1,8 +1,7 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
 #include "bmp.h"
-
 
 void grayscale(int height, int width, RGBTRIPLE image[height][width]);
 void sepia(int height, int width, RGBTRIPLE image[height][width]);
@@ -15,7 +14,8 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            int average = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0);
+            int average =
+                round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0);
             image[i][j].rgbtRed = average;
             image[i][j].rgbtGreen = average;
             image[i][j].rgbtBlue = average;
