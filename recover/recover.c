84e5a7ecb2c57a5c56ce9f1cd90026756729574e
risandiro <151243329+risandiro@users.noreply.github.com>
2024-05-10 15:04:43 +0000

    Fri, May 10, 2024, 5:04 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[])
 {
@@ -19,17 +20,19 @@ int main(int argc, char *argv[])
     }
 
     uint8_t buffer[512];
+
     unsigned int file_counter = 0;
+    bool new_jpeg = false;
+
 
     while(fread(buffer, 1, 512, card) == 512)
     {
-        if (buffer[0] == 0xff)
-            if (buffer[1] == 0xd8)
-                if (buffer[2] == 0xff)
-                    if ((buffer[3] & 0xf0) == 0xe0)
-                        {
-
-                        }
-
+        if ((buffer[0] == 0xff) && (buffer[1] == 0xd8) && (buffer[2] == 0xff) && ((buffer[3] & 0xf0) == 0xe0))
+            {
+                if (counter != 0)
+                {
+                    f
+                }
+            }
     }
 }
