fd71ca7e292439f2477aec9b857b60ee90bcca84
risandiro <151243329+risandiro@users.noreply.github.com>
2024-05-11 13:56:27 +0000

    Sat, May 11, 2024, 3:56 PM +02:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -38,7 +38,8 @@ int main(void)
 // Create a new individual with `generations`
 person *create_family(int generations)
 {
-    // TODO: Allocate memory for new person
+    //Allocate memory for new person
+    malloc(sizeof(person));
 
     // If there are still generations left to create
     if (generations > 1)
