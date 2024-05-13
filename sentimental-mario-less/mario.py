e185d19b92d0556420c9b9972b182e5a947a6bf0
risandiro <151243329+risandiro@users.noreply.github.com>
2024-05-13 19:17:12 +0000

    Mon, May 13, 2024, 9:17 PM +02:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -2,8 +2,13 @@ import sys
 
 def main():
     while True:
-        height = int(input("Height: "))
-        if 0 < height <= 8:
+        try:
+            height = int(input("Height: "))
+            if 0 < height <= 8:
+                break
+            raise
+        except:
+            print("Enter a number between 1 and 8 inclusive")
 
 
 
