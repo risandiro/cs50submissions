a395b95709058ad9d1098476b80d1642df0a515f
risandiro <151243329+risandiro@users.noreply.github.com>
2024-05-13 18:55:46 +0000

    Mon, May 13, 2024, 8:55 PM +02:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,4 +1,10 @@
 def main():
     height = int(input("Height: "))
     for i in range(height):
+        print_row(height)
+        height -= height
 
+def print_row(height: int):
+        print("#" * height)
+
+main()
