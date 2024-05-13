63c68f81240cb770c587dfddf26394c6bd8f1178
risandiro <151243329+risandiro@users.noreply.github.com>
2024-05-13 21:33:44 +0000

    Mon, May 13, 2024, 11:33 PM +02:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,5 +1,6 @@
 total = 0
 
+
 def main():
     while True:
         try:
@@ -14,12 +15,14 @@ def main():
     pennies(nickels(dimes(quarters(change))))
     print(total)
 
+
 def quarters(n: float) -> float:
     global total
     while n >= 0.25:
         n -= 0.25
         total += 1
     return round(n, 2)
+
 
 def dimes(n: float) -> float:
     global total
@@ -28,12 +31,14 @@ def dimes(n: float) -> float:
         total += 1
     return round(n, 2)
 
+
 def nickels(n: float) -> float:
     global total
     while n >= 0.05:
         n -= 0.05
         total += 1
     return round(n, 2)
+
 
 def pennies(n: float) -> float:
     global total
