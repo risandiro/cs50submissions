4574647d146cc38c98fef8e64b8c6aaf7982a713
risandiro <151243329+risandiro@users.noreply.github.com>
2024-05-13 20:37:16 +0000

    Mon, May 13, 2024, 10:37 PM +02:00

---
+++ sentimental-cash/cash.py
@@ -0,0 +1,13 @@
+def main():
+    while True:
+        try:
+            balance = float(input("Change: "))
+            if banance <= 0:
+                raise
+            break
+
+        except:
+            continue
+
+
+main()
