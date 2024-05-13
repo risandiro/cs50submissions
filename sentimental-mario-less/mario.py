bdb359ebeb9b99c1664da5f9c7c3d414d4199aba
risandiro <151243329+risandiro@users.noreply.github.com>
2024-05-13 19:04:13 +0000

    Mon, May 13, 2024, 9:04 PM +02:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -2,7 +2,7 @@ def main():
     height = int(input("Height: "))
     blank = int(1)
     while height > 0:
-        print(" " * (height - blank))
+        print(" " * (height - blank), end="")
         print("#" * blank)
         blank += 1
         height -= 1
