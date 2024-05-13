dbf0ab744755212420b86745e3e60ca0c61ec418
risandiro <151243329+risandiro@users.noreply.github.com>
2024-05-13 20:59:08 +0000

    Mon, May 13, 2024, 10:59 PM +02:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -13,15 +13,15 @@ def main():
     balance = change
 
     if balance >= 0.25:
-        int += balance / 0.25
+        total += balance // 0.25
         balance = balance % 0.25
 
     if balance >= 0.10:
-        total += balance / 0.10
+        total += balance // 0.10
         balance = balance % 0.10
 
     if balance >= 0.5:
-        total += balance / 0.5
+        total += balance // 0.5
         balance = balance % 0.5
 
     print(total + balance)
