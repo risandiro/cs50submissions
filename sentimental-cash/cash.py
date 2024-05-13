61d22d3d9ac5bbab28479e7d482190339ff6a2a4
risandiro <151243329+risandiro@users.noreply.github.com>
2024-05-13 21:24:24 +0000

    Mon, May 13, 2024, 11:24 PM +02:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -10,16 +10,37 @@ def main():
 
         except:
             continue
-        
-    dimes(change)
+
+    pennies(nickels(dimes(quarters(change))))
     print(total)
 
-
-def dimes(n: float):
+def quarters(n: float):
+    global total
     while n >= 0.25:
-        n - 0.25
+        n -= 0.25
         total += 1
     return n
+
+def dimes(n: float):
+    global total
+    while n >= 0.10:
+        n -= 0.10
+        total += 1
+    return n
+
+def nickels(n: float):
+    global total
+    while n >= 0.05:
+        n -= 0.05
+        total += 1
+    return n
+
+def pennies(n: float):
+    global total
+    while n >= 0.01:
+        n -= 0.01
+        total += 1
+    return
 
 
 main()
