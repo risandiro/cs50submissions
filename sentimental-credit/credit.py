6bf05dee15426907c962c59702e773da525455f0
risandiro <151243329+risandiro@users.noreply.github.com>
2024-05-17 16:13:10 +0000

    Fri, May 17, 2024, 6:13 PM +02:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -4,8 +4,16 @@ def main():
     if verify(number) != True:
         print("INVALID")
 
-    if len(number) == 15:
-        if 
+    if len(number) == 15 and number[0] == 3 and number[1] == 4 or number[1] == 7:
+        print("AMEX")
+
+    elif len(number) == 13 and number[0] == 4:
+        print("VISA")
+
+    elif len(number) == 16:
+        if number[0] == 4:
+            print("VISA")
+        elif number[0] == 5 and number[1] == 
 
 
 
