00f5d8735c7c5b49df2a6250a03436de378f1a29
risandiro <151243329+risandiro@users.noreply.github.com>
2024-05-21 16:21:02 +0000

    Tue, May 21, 2024, 6:21 PM +02:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,6 +1,6 @@
 def main():
     text = input("Text: ")
-    
+
 
 
 
@@ -10,6 +10,13 @@ def main():
 
 
     index = 0.0588 * L - 0.296 * S - 15.8
+
+def count_letters():
+    
+
+def count_words():
+
+def count_sentances():
 
 
 main()
