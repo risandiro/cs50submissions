53b189764bba9fc1183b6cfad1517b2fa63b2003
risandiro <151243329+risandiro@users.noreply.github.com>
2024-07-13 15:44:32 +0000

    Sat, Jul 13, 2024, 5:44 PM +02:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,1 +1,5 @@
 SELECT DISTINCT name FROM people
+JOIN directors ON people.id = directors.person_id
+JOIN movies ON directors.movie_id = movies.movie_id
+JOIN ratings ON movies.movie_id = ratings.movie_id
+WHERE movies.
