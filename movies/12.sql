2d6f10f337abeeba30cef343d6e7a5da37ecc9fc
risandiro <151243329+risandiro@users.noreply.github.com>
2024-07-13 16:26:45 +0000

    Sat, Jul 13, 2024, 6:26 PM +02:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,4 +1,14 @@
-SELECT title FROM movies
-JOIN stars ON movies.id = stars.movie_id
-JOIN people ON stars.person_id = people.id
-WHERE people.name = "Bradley Cooper"
+SELECT title FROM movies WHERE movies.id IN
+(
+    SELECT movies.id FROM movies
+    JOIN stars ON stars.movie_id = movies.id
+    JOIN people ON people.id = stars.person_id
+    WHERE people.name = "Bradley Cooper" AND movies.id IN
+    (
+        SELECT movies.id FROM movies
+        JOIN stars ON stars.movie_id = movies.id
+        JOIN people ON people.id = stars.person_id
+        WHERE people.name = "Jennifer Lawrence"
+    )
+);
+
