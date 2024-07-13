9878a000b899289d1a65fb0033525890828aa8d3
risandiro <151243329+risandiro@users.noreply.github.com>
2024-07-13 14:36:00 +0000

    Sat, Jul 13, 2024, 4:36 PM +02:00

--- movies/9.sql
+++ movies/9.sql
@@ -2,4 +2,4 @@ SELECT DISTINCT name FROM people
 JOIN stars ON people.id = stars.person_id
 JOIN movies ON stars.movie_id = movies.id
 WHERE movies.year = 2004
-ORDER BY birth;
+ORDER BY people.birth;
