632e6390257b7bc31884910d9aaf93f95f488067
risandiro <151243329+risandiro@users.noreply.github.com>
2024-07-13 14:41:03 +0000

    Sat, Jul 13, 2024, 4:41 PM +02:00

--- movies/8.sql
+++ movies/8.sql
@@ -1,4 +1,3 @@
-SELECT name FROM people
-JOIN stars ON people.id = stars.person_id
-JOIN movies ON stars.movie_id = movies.id
-WHERE movies.title = "Toy Story";
+SELECT name FROM people WHERE id IN
+ (SELECT person_id FROM stars WHERE movie_id IN
+  (SELECT id FROM movies WHERE title = "Toy Story"));
