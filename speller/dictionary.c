d6ca01c98cd442e0cbf855196fcbca6396d31928
risandiro <151243329+risandiro@users.noreply.github.com>
2024-07-13 11:22:21 +0000

    Sat, Jul 13, 2024, 1:22 PM +02:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -29,7 +29,7 @@ unsigned int hash_value;
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
-    int hash_value = hash(word);
+    hash_value = hash(word);
     node *cursor = table[hash_value];
 
     while(cursor != NULL)
