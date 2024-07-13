efa969ea7c910e5256a78c9080170cce7b7ca962
risandiro <151243329+risandiro@users.noreply.github.com>
2024-07-13 16:07:01 +0000

    Sat, Jul 13, 2024, 6:07 PM +02:00

--- movies/11.sql
+++ movies/11.sql
@@ -4,6 +4,7 @@ WHERE movies.id IN
     SELECT(movies.id) FROM movies
     JOIN stars ON movies.id = stars.movie_id
     JOIN ratings ON stars.movie_id = ratings.movie_id
+    JOIN people ON 
     WHERE stars.person_id = "Chadwick Boseman"
     ORDER BY ratings.rating
     LIMIT 5
