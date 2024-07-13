b16972bd0e655ad6bf53f1ab5263b8b1d08af7a2
risandiro <151243329+risandiro@users.noreply.github.com>
2024-07-13 15:51:08 +0000

    Sat, Jul 13, 2024, 5:51 PM +02:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,5 +1,8 @@
-SELECT DISTINCT name FROM people
-JOIN directors ON people.id = directors.person_id
-JOIN movies ON directors.movie_id = movies.id
-JOIN ratings ON movies.id = ratings.movie_id
-WHERE ratings.rating >= 9.0;
+SELECT name FROM people
+WHERE people.id IN
+(
+    JOIN directors ON people.id = directors.person_id
+    JOIN movies ON directors.movie_id = movies.id
+    JOIN ratings ON movies.id = ratings.movie_id
+    WHERE ratings.rating >= 9.0
+);
