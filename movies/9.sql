5098308688a89f814426beb1e262b740849779ae
risandiro <151243329+risandiro@users.noreply.github.com>
2024-07-13 14:47:37 +0000

    Sat, Jul 13, 2024, 4:47 PM +02:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,5 +1,4 @@
-SELECT DISTINCT name FROM people
-JOIN stars ON people.id = stars.person_id
-JOIN movies ON stars.movie_id = movies.id
-WHERE movies.year = 2004
+SELECT DISTINCT name FROM people WHERE id IN
+ (SELECT person_id FROM stars WHERE movie_id IN
+  (SELECT id FROM movies WHERE year = 2004))
 ORDER BY people.birth;
