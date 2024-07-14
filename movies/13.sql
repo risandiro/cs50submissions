d21a40d0fb76e56306be143a057ec44e172b3d50
risandiro <151243329+risandiro@users.noreply.github.com>
2024-07-14 13:20:53 +0000

    Sun, Jul 14, 2024, 3:20 PM +02:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,2 +1,11 @@
-SELECT name FROM people
+SELECT DISTINCT name FROM people
 JOIN stars ON people.id = stars.person_id
+JOIN movies ON stars.movie_id = movies.id
+WHERE movies.title IN
+(
+    SELECT DISTINCT(movies.title) FROM people
+    JOIN stars ON people.id = stars.person_id
+    JOIN movies ON stars.movie_id = movies.id
+)
+
+
