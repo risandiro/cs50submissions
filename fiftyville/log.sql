2691505eea4558b275f068d3398eea1f6685de80
risandiro <151243329+risandiro@users.noreply.github.com>
2024-07-14 13:45:14 +0000

    Sun, Jul 14, 2024, 3:45 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,6 +1,8 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 -- SELECT description FROM crime_scene_reports WHERE street = "Humphrey Street" AND day = "28" AND month = "7";
+-- Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted
+-- today with three witnesses who were present at the time – each of their interview transcripts
+-- mentions the bakery. Littering took place at 16:36. No known witnesses.
 
-SELECT description FROM crime_scene_reports WHERE street = "Humphrey Street" AND day = "28" AND month = "7";
-
+                                                                                                                                                               |
