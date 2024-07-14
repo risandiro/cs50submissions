cd305b7cd67c44e839ad0cee35687d72d7eb552d
risandiro <151243329+risandiro@users.noreply.github.com>
2024-07-14 15:00:34 +0000

    Sun, Jul 14, 2024, 5:00 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -176,4 +176,19 @@
 
 -- SELECT name FROM people WHERE id IN (SELECT person_id FROM bank_accounts WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE atm_location = "Leggett Street" AND month = "7" AND day = "28" AND transaction_type = "withdraw"));
 
---
+-- +---------+
+-- |  name   |
+-- +---------+
+-- | Kenny   |
+-- | Iman    |
+-- | Benista |
+-- | Taylor  |
+-- | Brooke  |
+-- | Luca    |
+-- | Diana   |
+-- | Bruce   |
+-- +---------+
+
+-- Suspects: Bruce
+
+------------------------------------------------------------------------------------------------------------------------------------
