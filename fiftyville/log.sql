79dbfe6cdd50119963fba490b23a1baeadc031e2
risandiro <151243329+risandiro@users.noreply.github.com>
2024-07-14 13:42:44 +0000

    Sun, Jul 14, 2024, 3:42 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,3 +1,6 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- SELECT description FROM crime_scene_reports WHERE street = "Humphrey Street" AND day = "28" AND month = "7";
+
+SELECT description FROM crime_scene_reports WHERE street = "Humphrey Street" AND day = "28" AND month = "7";
 
