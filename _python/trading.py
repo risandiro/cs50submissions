c5e16ef7c9df9744710413c69acf3bc4cf53c6ef
risandiro <151243329+risandiro@users.noreply.github.com>
2025-01-15 04:52:47 +0000

    Tue, Jan 14, 2025, 8:52 PM -08:00

--- _python/trading.py
+++ _python/trading.py
@@ -5,7 +5,7 @@ ticker = ticker.upper()
 entry, stoploss = float(entry), float(stoploss)
 
 if entry > stoploss:
-    target = entry - stoploss
+    target = entry + stoploss
 elif entry < stoploss:
     target = stoploss - entry
 
@@ -13,4 +13,4 @@ risk_2 = ((balance / 100) * 2) * target
 risk_15 = ((balance / 100) * 1.5) * target
 risk_1 = (balance / 100) * target
 
-print(f"{ticker} --> Target: {target + entry}  Entry: {entry}  Stoploss: {stoploss}    Risk? 2%: {risk_2} 1.5%: {risk_15} 1%: {risk_1}")
+print(f"{ticker} --> Target: {target + entry}  Entry: {entry}  Stoploss: {stoploss}    Risk? 2%: -{risk_2}- 1.5%: -{risk_15}- 1%: -{risk_1}-")
