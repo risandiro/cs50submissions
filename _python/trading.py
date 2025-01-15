b27693bad47da4dbc31555c951b99b61774094d4
risandiro <151243329+risandiro@users.noreply.github.com>
2025-01-15 05:02:14 +0000

    Tue, Jan 14, 2025, 9:02 PM -08:00

--- _python/trading.py
+++ _python/trading.py
@@ -1,16 +1,17 @@
 balance = int(input("Account Balance: "))
-ticker, entry, stoploss = input("Ticker---Entry---Stoploss? ").split()
+while True:
+    ticker, entry, stoploss = input("Ticker---Entry---Stoploss? ").split()
 
-ticker = ticker.upper()
-entry, stoploss = float(entry), float(stoploss)
+    ticker = ticker.upper()
+    entry, stoploss = float(entry), float(stoploss)
 
-if entry > stoploss:
-    target = entry + stoploss
-elif entry < stoploss:
-    target = entry - (stoploss - entry)
+    if entry > stoploss:
+        target = entry + stoploss
+    elif entry < stoploss:
+        target = entry - (stoploss - entry)
 
-risk_2 = ((balance / 100) * 2) * target
-risk_15 = ((balance / 100) * 1.5) * target
-risk_1 = (balance / 100) * target
+    risk_2 = ((balance / 100) * 2) * target
+    risk_15 = ((balance / 100) * 1.5) * target
+    risk_1 = (balance / 100) * target
 
-print(f"{ticker} --> Target: {target}  Entry: {entry}  Stoploss: {stoploss}    Risk? 2%: -{risk_2}- 1.5%: -{risk_15}- 1%: -{risk_1}-")
+    print(f"{ticker} --> Target: {target}  Entry: {entry}  Stoploss: {stoploss}    Risk? 2%: -{risk_2}- 1.5%: -{risk_15}- 1%: -{risk_1}-")
