720e2977989fd107163f3c0f0a926b454d8cbbab
risandiro <151243329+risandiro@users.noreply.github.com>
2025-04-13 03:34:21 +0000

    Sat, Apr 12, 2025, 8:34 PM -07:00

--- _python/flask/hello/app.py
+++ _python/flask/hello/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request
 
-# treat this file as a web application
+# initialization of flask, treat this file as a web application
 app = Flask(__name__)
 
-# defining a route for this application, whenever a human visits slash on this
-# application, what should happen is this function index should get called
+
+''' defining a route for this application, whenever a user visits slash on this
+application, what should happen is this function index should get called. Index
+function uses render_template to return a index.html back to the user '''
 @app.route("/")
 def index():
-    return "hello, world"
+    return render_template("index.html")
