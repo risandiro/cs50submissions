54a8e06fa8d2e077b51f13e792acc3c5237625d2
risandiro <151243329+risandiro@users.noreply.github.com>
2025-04-13 03:17:14 +0000

    Sat, Apr 12, 2025, 8:17 PM -07:00

--- _python/flask/app.py
+++ _python/flask/app.py
@@ -2,3 +2,8 @@ from flask import Flask, render_template, request
 
 # treat this file as a web application
 app = Flask(__name__)
+
+# defining a route for this application, whenever
+@app.route("/")
+def index():
+    return "hello, world"
