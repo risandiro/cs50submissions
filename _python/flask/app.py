4dd9a5b95df7c452fc04588891d55d949cbb2855
risandiro <151243329+risandiro@users.noreply.github.com>
2025-04-13 03:19:25 +0000

    Sat, Apr 12, 2025, 8:19 PM -07:00

--- _python/flask/app.py
+++ _python/flask/app.py
@@ -3,7 +3,8 @@ from flask import Flask, render_template, request
 # treat this file as a web application
 app = Flask(__name__)
 
-# defining a route for this application, whenever
+# defining a route for this application, whenever a human visits slash on this
+# application, what should happen is this function index should get called
 @app.route("/")
 def index():
     return "hello, world"
