f8368b5963c212e0eaee7ae0603381df71adb872
risandiro <151243329+risandiro@users.noreply.github.com>
2025-04-21 01:57:13 +0000

    Sun, Apr 20, 2025, 6:57 PM -07:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -26,7 +26,9 @@ def after_request(response):
 def index():
     if request.method == "POST":
 
-        # TODO: Add the user's entry into the database
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
 
         return redirect("/")
 
