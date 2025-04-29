99a013794a6906bbefba0f50182a624de3219cf8
risandiro <151243329+risandiro@users.noreply.github.com>
2025-04-29 03:34:22 +0000

    Mon, Apr 28, 2025, 8:34 PM -07:00

--- finance/app.py
+++ finance/app.py
@@ -57,6 +57,8 @@ def buy():
 
         # validate if user can afford the purchase
         user_cash = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])
+        user_cash = user_cash[0]
+        print(user_cash)
         total_cost = quote["price"] * quantity
         if not int(total_cost) <= user_cash:
             return apology("not enough cash for the purchase", 403)
